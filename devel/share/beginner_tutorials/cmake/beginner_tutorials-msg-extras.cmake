aaf54f94f91a28d80ff4bd4376c885198e576221
susaneileenfox <fox@macalester.edu>
2016-06-03 11:12:48 -0500

    deleting devel/share/Foxbeginner

--- devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(beginner_tutorials_MESSAGE_FILES "/home/macalester/Desktop/githubRepositories/catkin_ws/src/beginner_tutorials/msg/Num.msg")
-set(beginner_tutorials_SERVICE_FILES "/home/macalester/Desktop/githubRepositories/catkin_ws/src/beginner_tutorials/srv/AddTwoInts.srv")
--- devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(beginner_tutorials_MSG_INCLUDE_DIRS "/home/macalester/Desktop/githubRepositories/catkin_ws/src/beginner_tutorials/msg")
-set(beginner_tutorials_MSG_DEPENDENCIES std_msgs)
