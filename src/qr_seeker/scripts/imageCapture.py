f4795b3bca6041b14df3bcb72c622a10946039c2
susaneileenfox <fox@macalester.edu>
2016-06-03 11:55:48 -0500

    Ran imageCapture and made necessary changes

--- src/qr_seeker/scripts/imageCapture.py
+++ src/qr_seeker/scripts/imageCapture.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import TurtleBot
+import turtleQR
 import cv2
 from datetime import datetime
 from collections import deque
@@ -11,12 +11,8 @@ import time
 class ImageCapture(object):
 
 	def __init__(self):
-        self.robot = TurtleBot.TurtleBot()
-		image, times = self.robot.getImage()		
-
-		self.camera = UpdateCamera(self.robot)
-
-		self.lock = threading.Lock()
+		self.robot = turtleQR.TurtleBot()
+		image, times = self.robot.getImage()
 		self.runFlag = True
 		self.stalled = False
 		self.frameAverageStallThreshold = 20
@@ -30,37 +26,32 @@ class ImageCapture(object):
 
 			image, timesImageServed = self.robot.getImage()
 
-			with self.lock:
-				if timesImageServed > 20:
-					if self.stalled == False:
-						print "Camera Stalled!"
-					self.stalled = True
-				else:
-					self.stalled = False
+			if timesImageServed > 20:
+				if self.stalled == False:
+					print "Camera Stalled!"
+				self.stalled = True
+			else:
+				self.stalled = False
 			
 			cv2.imshow("TurtleCam", image)
 
 			code = chr(cv2.waitKey(50) & 255)
 
 			if code == 't':
-				cv2.imwrite("/home/macalester/catkin_ws/src/speedy_nav/res/captures/cap-" 
+				cv2.imwrite("/home/macalester/catkin_ws/src/qr_seeker/res/captures/cap-" 
 					+ time.strftime("%b%d%a-%H%M%S.jpg"), image)
 				print "Image saved!"
 			if code == 'q':
 				break
 
-			with self.lock:
-				runFlag = self.runFlag
+			runFlag = self.runFlag
 
 	def isStalled(self):
 		"""Returns the status of the camera stream"""
-		with self.lock:
-			stalled = self.stalled
-		return stalled
+		return self.stalled
 
 	def exit(self):
-		with self.lock:
-			self.runFlag = False
+		self.runFlag = False
 	
 if __name__=="__main__":
 	rospy.init_node('imageCapture')
