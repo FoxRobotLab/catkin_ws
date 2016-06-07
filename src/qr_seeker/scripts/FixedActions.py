4a3a1dcb55e747beacfa66fce3b1bc24b6426b49
rhohle <rhohle@macalester.edu>
2016-06-07 16:09:45 -0500

    hardcode 3

--- src/qr_seeker/scripts/FixedActions.py
+++ src/qr_seeker/scripts/FixedActions.py
@@ -100,7 +100,8 @@ class FixedActions(object):
         if self.camera.isStalled():
             return
         print 'Turning by an angle of: ', str(angle)
-        turnSec = angle
+        turnSec = angle * self.d2s
+        turnSec = 3
         if angle < 0:
             turnSec = abs(turnSec)
             self.robot.turnLeft(0.4, turnSec)
