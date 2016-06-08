72ba1d1c912f8c4a7cc328bd449383f6378e7235
mjulmer <mulmer@macalester.edu>
2016-06-08 16:18:10 -0500

    more git grossness removal

--- src/qr_seeker/scripts/UpdateCamera.py
+++ src/qr_seeker/scripts/UpdateCamera.py
@@ -69,57 +69,11 @@ class UpdateCamera( threading.Thread ):
             with self.lock:
                 runFlag = self.runFlag
 
-<<<<<<< HEAD
     # def orbScan(self, image):
     #     result = self.orbScanner.scanImage(image)
     #     #if result is none then orbScanner did not find enough points
     #     with self.lock:
     #         self.orbInfo = result
-
-=======
-    def orbScan(self, image):
-        result = self.orbScanner.scanImage(image)
-        #if result is none then orbScanner did not find enough points
-        with self.lock:
-            self.orbInfo = result
-
-    def qrScan(self, image):
-        self.qrScanner.parse_config('enable')
-        bwImg = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        #cv2.imshow("bwimg", bwImg)
-        #cv2.waitKey(0)
-        pil_im = Image.fromarray(bwImg)
-        pic2 = pil_im.convert("L")
-        wid, hgt = pic2.size
-        #print("wid, hgt,", wid, hgt)
-        raw = pic2.tobytes()
-
-        img = zbar.Image(wid, hgt, 'Y800', raw)
-        result = self.qrScanner.scan(img)
-        #print "RESULT", result
-        if result == 0:
-            #print "Scan failed"
-            pass
-        else:
-            #print ("img is ", img)
-            for symbol in img:
-                #print "symbol did indeed get assigned"
-                pass
-            del(img)
-            codeData = symbol.data.decode(u'utf-8')
-            #print "Data found:", codeData
-            list = string.split(codeData)
-            #print(list)
-            nodeNum = list[0]
-            nodeCoord = list[1] + ' ' + list[2]
-            nodeName = ''
-            for i in range(3, len(list)):
-                nodeName = nodeName + ' ' + list[i]
-
-            #nodeNum, nodeCoord, nodeName = string.split(codeData)
-            with self.lock:
-                self.qrInfo = (int(nodeNum), nodeCoord, nodeName)
->>>>>>> 249cfe24a22de747a3c854a1e65a8209038642e2
 
     def isStalled(self):
         """Returns the status of the camera stream"""
