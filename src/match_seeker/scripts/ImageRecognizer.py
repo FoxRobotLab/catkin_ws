263389958718f8a0a3d10db561b85bac76a3a3ca
susaneileenfox <fox@macalester.edu>
2017-05-23 14:43:31 -0500

    Integrating ImageRecognizer into qrPlanner in match_seeker

--- src/match_seeker/scripts/ImageRecognizer.py
+++ src/match_seeker/scripts/ImageRecognizer.py
@@ -187,7 +187,7 @@ class ImageMatcher(object):
     def getFileByNumber(self, fileNum):
         """Makes a filename given the number and reads in the file, returning it."""
         filename = self.makeFilename(fileNum)
-        print "getFileByNumber filename=", filename
+        image = cv2.imread(filename)
         if image is None:
             print("Failed to read image:", filename)
         return image
