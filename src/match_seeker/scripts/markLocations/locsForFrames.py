170309c72720b413870368a13452063f72a0758b
susaneileenfox <fox@macalester.edu>
2017-05-25 12:34:33 -0500

    Adding location data for Feb 2017 image dataes

---
+++ src/match_seeker/scripts/markLocations/locsForFrames.py
@@ -0,0 +1,349 @@
+
+
+import time
+import os
+import cv2
+import numpy as np
+import readMap
+
+class LabeledFrames(object):
+
+    def __init__(self, mapFile, data, mode = "video"):
+        """Set up data to be held, including displayed and stored maps, current labeling location
+        and heading, and dictionary mapping frame numbers to locations and headings. Default mode
+        is to run on a stored video feed, but it can also run through a folder of images. The "data" field
+        is either the name of the video or of the folder where the images are found. The mode variable is either
+        'video' or 'images'"""
+        self.currLoc = (0, 0)
+        self.currHeading = 0
+        self.mode = mode
+
+        # variables for managing data source
+        self.cap = None     # only used if mode = video
+        self.imgFileList = []    # only used if mode = 'images'
+        self.picNum = -1
+        self.mapFilename = mapFile
+        self.dataSource = data
+        self.dataDone = False
+
+        # instance variables to hold displayed images
+        self.mainImg = None
+        self.origMap = None
+        self.currMap = None
+        self.currFrame = None
+
+        # Instance variables to hold outcome data
+        self.labeling = dict()
+
+
+    def go(self):
+        """Run the program, setting up windows and all."""
+        self._setupWindows()
+
+        # set up main window and callback
+        self.mainImg = self._buildMainImage()
+        self._displayStatus()
+        cv2.imshow("Main", self.mainImg)
+        cv2.setMouseCallback("Main", self._mouseMainResponse)
+
+        # set up map window and callback
+        self.origMap = self._getOlinMap()
+        (self.mapHgt, self.mapWid, dep) = self.origMap.shape
+        self.currMap = self.origMap
+        cv2.imshow("Map", self.currMap)
+        cv2.setMouseCallback("Map", self._mouseSetLoc)
+
+        # set up video capture and first frame
+        self._setupImageCapture()
+        ch = ' '
+        goodFrame = self._getNextImage()
+        if not goodFrame:
+            self.dataDone = True
+        cv2.putText(self.currFrame, str(self.picNum), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0), 2)
+
+        # run main loop until user quits or run out of frames
+        while (not self.dataDone) and (ch != 'q'):
+            cv2.imshow("Image", self.currFrame)
+            x = cv2.waitKey(0)
+            ch = chr(x & 0xFF)
+            if ch in "wasd" and self.currLoc != (0, 0):  # if robot location has been placed on the map
+                (mapX, mapY) = self._convertWorldToMap(self.currLoc[0], self.currLoc[1])
+                if ch == 'a':
+                    mapX -= 2
+                elif ch == 'd':
+                    mapX += 2
+                elif ch == 'w':
+                    mapY -= 2
+                elif ch == 's':
+                    mapY += 2
+                self.currLoc = self._convertMapToWorld(mapX, mapY)
+                self._updateMap( (mapX, mapY) )
+
+        # close things down nicely
+
+        self._writeData()
+        self._cleanUp()
+        cv2.destroyAllWindows()
+
+
+    def _writeData(self):
+        """Write the data collected to a timestamped file."""
+        try:
+            os.makedirs('../../res/locdata/')
+        except:
+            pass
+        logName = time.strftime("Data-%b%d%a-%H%M%S.txt")
+        print(logName)
+        fileOpen = False
+        logFile = None
+        try:
+            logFile = open("../../res/locdata/" + logName, 'w')
+            fileOpen = True
+        except:
+            print ("FAILED TO OPEN DATA FILE")
+
+        for picNum in self.labeling:
+            [x, y, h] = self.labeling[picNum]
+            dataStr = str(picNum) + " " + str(x) + " " + str(y) + " " + str(h) + "\n"
+            if fileOpen:
+                logFile.write(dataStr)
+            else:
+                print("Frame", picNum, "with location", (x, y, h))
+        logFile.close()
+
+
+    def _setupWindows(self):
+        "Creates three windows and moves them to the right places on screen."
+        cv2.namedWindow("Main")
+        cv2.namedWindow("Image")
+        cv2.namedWindow("Map")
+        cv2.moveWindow("Main", 30, 50)
+        cv2.moveWindow("Image", 30, 550)
+        cv2.moveWindow("Map", 700, 50)
+
+
+    def _buildMainImage(self):
+        """Builds the main window image with buttons at given locations. Note that space is left for
+        displaying the robot's current location, but that is done by a separate method."""
+
+        yellow = (0, 255, 255)
+        cyan = (255, 255, 0)
+        green = (0, 255, 0)
+
+        newMain = np.zeros((420, 210, 3), np.uint8)
+
+        # Put squares for NE, SE, SW, NW
+        info = [[0, 0, "NW 45"], [0, 140, "SW 135"], [140, 0, "NE 315"], [140, 140, "SE 225"]]
+        for square in info:
+            (x, y, strng) = square
+            cv2.rectangle(newMain, (x, y), (x + 69, y + 69), yellow, -1)
+            cv2.putText(newMain, strng, (x + 20, y + 40), cv2.FONT_HERSHEY_PLAIN, 0.8, (0, 0, 0))
+
+        # Put squares for N, S, E, W
+        info = [[70, 0, "N 0"], [70, 140, "S 180"], [0, 70, "W 90"], [140, 70, "E 270"]]
+        for square in info:
+            (x, y, strng) = square
+            cv2.rectangle(newMain, (x, y), (x + 69, y + 69), cyan, -1)
+            cv2.putText(newMain, strng, (x + 20, y + 40), cv2.FONT_HERSHEY_PLAIN, 0.8, (0, 0, 0))
+
+        cv2.rectangle(newMain, (60, 350), (149, 400), green, -1)
+        cv2.putText(newMain, "Next Frame", (65, 380), cv2.FONT_HERSHEY_PLAIN, 0.8, (0, 0, 0))
+        return newMain
+
+
+    def _displayStatus(self):
+        """Displays the current location and frame counter on the main window."""
+        yellow = (0, 255, 255)
+        cv2.rectangle(self.mainImg, (0, 210), (210, 340), (0, 0, 0), -1)
+        floatTemplate = "{0:s} = {1:^6.2f}"
+        intTemplate = "{0:s} = {1:^3d}"
+        xStr = floatTemplate.format('x', self.currLoc[0])
+        yStr = floatTemplate.format('y', self.currLoc[1])
+        hStr = intTemplate.format('h', self.currHeading)
+        countStr = intTemplate.format('frame', self.picNum)
+        cv2.putText(self.mainImg, "Current location:", (20, 240), cv2.FONT_HERSHEY_PLAIN, 1.0, yellow)
+        cv2.putText(self.mainImg, xStr, (30, 260), cv2.FONT_HERSHEY_PLAIN, 1.0, yellow)
+        cv2.putText(self.mainImg, yStr, (30, 280), cv2.FONT_HERSHEY_PLAIN, 1.0, yellow)
+        cv2.putText(self.mainImg, hStr, (30, 300), cv2.FONT_HERSHEY_PLAIN, 1.0, yellow)
+        cv2.putText(self.mainImg, countStr, (20, 330), cv2.FONT_HERSHEY_PLAIN, 1.0, yellow)
+        cv2.imshow("Main", self.mainImg)
+
+
+    def _mouseMainResponse(self, event, x, y, flags, param):
+        """A mouse callback function that reads the user's click and responds by updating the robot's heading,
+        or by moving on to the next frame in the video."""
+
+        if event == cv2.EVENT_LBUTTONDOWN:
+            if (0 <= x < 70) and (0 <= y < 70):
+                # click was in "northwest" heading square
+                self.currHeading = 45
+            elif (0 <= x < 70) and (70 <= y < 140):
+                # click was in "west" heading square
+                self.currHeading = 90
+            elif (0 <= x < 70) and (140 <= y < 210):
+                # click was in "southwest" heading square
+                self.currHeading = 135
+            elif (70 <= x < 140) and (0 <= y < 70):
+                # click was in "north" heading square
+                self.currHeading = 0
+            elif (70 <= x < 140) and (140 <= y < 210):
+                # click was in "south" heading square
+                self.currHeading = 180
+            elif (140 <= x < 210) and (0 <= y < 70):
+                # click was in "northeast" heading square
+                self.currHeading = 315
+            elif (140 <= x < 210) and (70 <= y < 140):
+                # click was in "east" heading square
+                self.currHeading = 270
+            elif (140 <= x < 210) and (140 <= y < 210):
+                # click was in "southeast" heading square
+                self.currHeading = 225
+            elif (60 <= x < 150) and (350 <= y <= 400):
+                self.labeling[self.picNum] = [self.currLoc[0], self.currLoc[1], self.currHeading]
+
+                goodFrame = self._getNextImage()
+                if not goodFrame:
+                    print("WHOOPS!, no image found!")
+                    self.dataDone = True
+                else:
+                    cv2.putText(self.currFrame, str(self.picNum), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0), 2)
+                    cv2.imshow("Image", self.currFrame)
+            self._displayStatus()
+
+
+    def _getOlinMap(self):
+        """Read in the Olin Map and return it. Note: this has hard-coded the orientation flip of the particular
+        Olin map we have, which might not be great, but I don't feel like making it more general. Future improvement
+        perhaps."""
+        origMap = readMap.createMapImage(self.mapFilename, 20)
+        map2 = np.flipud(origMap)
+        olinMap = np.rot90(map2)
+        return olinMap
+
+
+    def _mouseSetLoc(self, event, x, y, flags, param):
+        """A mouse callback function that reads the user's click location and marks it on the map, also making it
+        available to the program to use."""
+
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.currLoc = self._convertMapToWorld(x, y)
+            print "mouseSetLoc self.currLoc =", self.currLoc
+            self._updateMap( (x, y) )
+
+
+    def _updateMap(self, mapLoc):
+        self.currMap = self._highlightLocation(mapLoc, "pixels")
+        cv2.imshow("Map", self.currMap)
+        self._displayStatus()
+
+
+    def _highlightLocation(self, currPos, units = "meters"):
+        """Hightlight the robot's current location based on the input.
+        currPos is given in meters, so need to convert: 20 pixels per meter."""
+        newMap = self.origMap.copy()
+        if units == "meters":
+            (currX, currY) = currPos
+            (mapX, mapY) = self._convertWorldToMap(currX, currY)
+        else:
+            (mapX, mapY) = currPos
+        cv2.circle(newMap, (mapX, mapY), 6, (0, 0, 255))
+        return newMap
+
+
+    def _convertMapToWorld(self, mapX, mapY):
+        """Converts coordinates in pixels, on the map, to coordinates (real-valued) in
+        meters. Note that this also has to adjust for the rotation and flipping of the map."""
+        # First flip x and y values around...
+        flipY = self.mapWid - 1 - mapX
+        flipX = self.mapHgt - 1 - mapY
+        # Next convert to meters from pixels, assuming 20 pixels per meter
+        mapXMeters = flipX / 20.0
+        mapYMeters = flipY / 20.0
+        return (mapXMeters, mapYMeters)
+
+
+    def _convertWorldToMap(self, worldX, worldY):
+        """Converts coordinates in meters in the world to integer coordinates on the map
+        Note that this also has to adjust for the rotation and flipping of the map."""
+        # First convert from meters to pixels, assuming 20 pixels per meter
+        pixelX = worldX * 20.0
+        pixelY = worldY * 20.0
+        # Next flip x and y values around
+        mapX = self.mapWid - 1 - pixelY
+        mapY = self.mapHgt - 1 - pixelX
+        return (int(mapX), int(mapY))
+
+
+
+    def _setupImageCapture(self):
+        """Sets up the video capture for the given filename, printing a message if it failed
+        and returning None in that case."""
+        if self.mode == 'video':
+            try:
+                self.cap = cv2.VideoCapture(self.dataSource)
+            except:
+                print("Capture failed!")
+                self.cap = None
+        else:
+            self.imgFileList = os.listdir(self.dataSource)
+            self.imgFileList.sort()
+
+
+    def _getNextImage(self):
+        """Gets the next frame from the camera or from reading the next file"""
+        if self.mode == 'video':
+            good, frame = self.cap.read()
+            if not good:
+                return good
+            self.picNum += 1
+            self.currFrame = frame
+            return good
+        else:
+            while True:
+                if self.imgFileList == []:
+                    return False
+                filename = self.imgFileList[0]
+                self.imgFileList.pop(0)
+                if filename[-3:] in {"jpg", "png"}:
+                    try:
+                        newIm = cv2.imread(self.dataSource + filename)
+                    except IOError:
+                        return False
+                    thisNum = self._extractNum(filename)
+                    self.currFrame = newIm
+                    self.picNum = thisNum
+                    return True
+
+
+
+    def _extractNum(self, fileString):
+        """finds sequence of digits"""
+        numStr = ""
+        foundDigits = False
+        for c in fileString:
+            if c in '0123456789':
+                foundDigits = True
+                numStr += c
+            elif foundDigits == True:
+                break
+        if numStr != "":
+            return int(numStr)
+        else:
+            self.picNum += 1
+            return self.picNum
+
+
+    def _cleanUp(self):
+        """Closes down video capture, if necessary"""
+        if self.mode == 'video':
+            self.cap.release()
+
+
+
+if __name__ == "__main__":
+    frameRecorder = LabeledFrames("olinNewMap.txt", "/Users/susan/PycharmProjects/markLocations/Feb7B.avi")
+    frameRecorder.go()
+
+
+    frameRecorder = LabeledFrames("olinNewMap.txt", "../../res/May2517/", "images")
+    frameRecorder.go()
---
+++ src/match_seeker/scripts/markLocations/readMap.py
@@ -0,0 +1,134 @@
+
+"""
+File: readMap.py
+Author: Susan Fox
+Date: Februar 2017
+
+This file contains code to read in the Olin-Rice map data from a file and construct
+an image of a chosen scale for the map data.
+"""
+
+import cv2
+import numpy as np
+
+
+def createMapImage(filename, picUnits = 20):
+    """Takes in a filename containing map information, and the number of pixels per meter in the final image,
+    and it reads in the data from the file, and creates an image of the map accordingly."""
+    params, lines = inputMap(filename)
+    mapImage = drawMap(params, lines, params['scale'], picUnits)
+    return mapImage
+
+
+def inputMap(filename):
+    """Given a filename, it reads in the data about the map from the file, returning the parameters of the map
+    and a list of the lines specified in it."""
+    fil = open(filename, 'r')
+    parameters = readHeader(fil)
+    lineList = readLines(fil)
+    fil.close()
+    return parameters, lineList
+
+
+def readHeader(fil):
+    """Read in the header information, which includes the size of the map, its scale, and
+    how many lines make up its walls."""
+    # Loop until you see the LINES line
+    params = dict()
+    while True:
+        nextLine = fil.readline()
+        nextLine = nextLine.strip()
+        # If reach the end of the header section, stop
+        if nextLine == "LINES" or nextLine == "":
+            break
+        lineWords = nextLine.split()
+        if lineWords[0] == "LineMinPos:":
+            params["minPos"] = [int(v) for v in lineWords[1:]]
+        elif lineWords[0] == "LineMaxPos:":
+            params["maxPos"] = [int(v) for v in lineWords[1:]]
+        elif lineWords[0] == "NumLines:":
+            params["numLines"] = [int(v) for v in lineWords[1:]]
+        elif lineWords[0] == "Scale:":
+            params["scale"] = int(lineWords[1])
+
+    return params
+
+
+def readLines(fil):
+    """Read in the lines, making a list of them. Each line is defined by four values, forming
+    two points, which are the endpoints of the line."""
+    lines = []
+    biggestY = 0
+    biggestX = 0
+    while True:
+        nextText = fil.readline()
+        nextText = nextText.strip()
+        # If read the end of the lines, stop
+        if nextText == "DATA" or nextText == "":
+            break
+        elif nextText[0] == '#':  # line is a comment, skip it
+            continue
+        else:
+            nextLine = [int(v) for v in nextText.split()]
+            if nextLine[0] > biggestX:
+                biggestX = nextLine[0]
+            if nextLine[2] > biggestX:
+                biggestX = nextLine[2]
+            if nextLine[1] > biggestY:
+                biggestY = nextLine[1]
+            if nextLine[3] > biggestY:
+                biggestY = nextLine[3]
+            lines.append(nextLine)
+    return lines
+
+
+def drawMap(parameters, lines, mapUnits = 1000, picUnits = 20):
+    """This should use the parameters from the map to make a picture of the map.
+     It scales the map according to the  3rd and 4th inputs, which tell how many of
+     the base units in the map should be converted into how many pixel in the picture."""
+    [minX, minY] = parameters["minPos"]
+    [maxX, maxY] = parameters["maxPos"]
+    widthMap = maxX - minX + 1
+    heightMap = maxY - minY + 1
+    widthPic = convertToPixels(widthMap, mapUnits, picUnits)
+    heightPic = convertToPixels(heightMap, mapUnits, picUnits)
+    mapImage = 255 * np.ones((heightPic, widthPic, 3), np.uint8)
+
+    # drawGrid(mapImage, heightPic, widthPic, picUnits)
+
+    for line in lines:
+        col = (0, 0, 0)
+        [x1, y1, x2, y2] = [convertToPixels(val, mapUnits, picUnits) for val in line]
+        cv2.line(mapImage, (x1, y1), (x2, y2), col, 1)
+    return mapImage
+
+
+def convertToPixels(value, mapUn, picUn):
+    """Convert the value in map units into pixels using the given scale."""
+    return int(np.ceil((value / mapUn) * picUn))
+
+
+def drawGrid(map, hgt, wid, units):
+    """Draw horizontal and vertical lines marking each square meter on the picture."""
+    for x in range(0, wid, units):
+        if (x % 100 == 0):
+            lineCol = (0, 255, 255)
+        else:
+            lineCol = (255, 255, 0)
+        cv2.line(map, (x, 0), (x, hgt), lineCol)
+    for y in range(0, hgt, units):
+        if (y % 100 == 0):
+            lineCol = (0, 255, 255)
+        else:
+            lineCol = (255, 255, 0)
+        cv2.line(map, (0, y), (wid, y), lineCol)
+
+
+if __name__ == "__main__":
+    map = createMapImage("olinNewMap.txt", 20)
+    cv2.imshow("test map", map)
+    print(map.shape)
+    map2 = np.flipud(map)
+    map3 = np.rot90(map2)
+    cv2.imshow("map2", map3)
+    cv2.waitKey(0)
