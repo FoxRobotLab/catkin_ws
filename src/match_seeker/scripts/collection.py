fc991d6e207a604ba2309bf8ff976d5e4306d406
susaneileenfox <fox@macalester.edu>
2017-06-01 09:45:49 -0500

    Clean up ignored files (removing videos from repository)

---
+++ src/match_seeker/scripts/collection.py
@@ -0,0 +1,49 @@
+
+
+import os
+from OSPathDefine import basePath
+import cv2
+
+
+currDirectory = basePath + "res/refinedFeb2017Data/"
+baseName = "frame"
+extension = "jpg"
+
+
+
+def makeCollection():
+    """Reads in all the images in the specified directory, start number and end number, and
+    makes a list of ImageFeature objects for each image read in."""
+    if (currDirectory is None):
+        print("ERROR: cannot run makeCollection without a directory")
+        return
+
+
+    listDir = os.listdir(currDirectory)
+
+    for file in listDir:
+        pic = cv2.imread(currDirectory + file)
+        end = len(file)-(len(extension) + 1)
+        picNum = int(file[len(baseName):end])
+        # cv2.imshow("pic", pic)
+        # cv2.waitKey(0)
+
+
+        # features = ImageFeatures.ImageFeatures(image, picNum, self.logger, self.ORBFinder)
+        # self.featureCollection[picNum] = features
+
+        # if i % 100 == 0:
+        #     print i
+
+
+makeCollection()
+
+
+
+
+
+
+
+
+
+
