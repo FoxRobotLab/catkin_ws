fa2e4b0a6adba7ca61f3bf7807b373482ccd3e5a
susaneileenfox <fox@macalester.edu>
2017-06-07 16:23:25 -0500

    Added more to the create database

--- src/match_seeker/scripts/OSPathDefine.py
+++ src/match_seeker/scripts/OSPathDefine.py
@@ -6,12 +6,12 @@ computers more easily."""
 # base path on Enterprise
 basePath = "/home/macalester/catkin_ws/src/match_seeker/"
 # Kobuki
-directory = "res/kobuki0530Pics/"
-locData = "res/locations/kobuki0530.txt"
+# directory = "res/kobuki0530Pics/"
+# locData = "res/locations/kobuki0530.txt"
 
 ## Create
-# directory = "res/create060717/"
-# locData = "res/locations/create0607.txt"
+directory = "res/create060717/"
+locData = "res/locations/create0607.txt"
 
 # base path on FoxVoyager (aka Speedy)
 # basePath = "/home/macalester/Desktop/githubRepositories/catkin_ws/src/match_seeker/"
