99fc5afc34a549436c849167aaa20bf544e1c568
jlim2 <jlim2@macalester.edu>
2018-05-30 16:56:03 -0500

    Modified cullPictures to handle non-jpg files and not show image numbers on DIFF. Also added documentation. Added .idea/ to .gitignore

--- src/match_seeker/scripts/buildingDatabases/cullPictures.py
+++ src/match_seeker/scripts/buildingDatabases/cullPictures.py
@@ -2,6 +2,20 @@ import os
 import cv2
 import numpy as np
 
+""" ------------------------------------------------------------------------------------------------------------------
+File: cullPictures.py
+Date: May 2018
+
+For identifying (blur or duplicate) images that need to be deleted.
+- Displays currrent, (undeleted) previous, and absolute difference (a.k.a. DIFF) of the two images
+- By looking at those images (mainly DIFF for duplicate and current for blur), user decides whether or not the current
+image should be deleted.
+- Has "undo" functionality that lets the user to go backward by one image
+- More graceful handling of missing images (discrete image (or frame) numbers that might have been due to manual
+deletion of some images) compared to scanImageMatches.py
+- Make sure to cut images by 10000 if there are more than that. This is to prevent the image *jumping* from 1000 to
+10000 (then 10009 to 1001 and etc.) because the minimum number of digits when saving frames is 4
+------------------------------------------------------------------------------------------------------------------"""
 
 class CullPictures(object):
     def __init__(self, imageDir, outputFileName, startFileName = ""):
@@ -12,7 +26,7 @@ class CullPictures(object):
         self.filenames = os.listdir(self.imageDir)
         self.filenames.sort()
 
-        # set up windows
+        # Set up windows
         cv2.namedWindow("current")
         cv2.namedWindow("previous")
         cv2.namedWindow("DIFF")
@@ -31,10 +45,15 @@ class CullPictures(object):
 
         currPrevDic = dict()
         currPrevDic[self.filenames[i]] = None
+
         while (i < len(self.filenames) and i >= 0):
             currFile = self.filenames[i]
+
+            # Skips the non-jpg files.
             if (not currFile.endswith("jpg")):
                 print("Non-jpg file in folder: ", currFile, "... skipping!")
+                i += 1
+                currPrevDic[self.filenames[i]] = None
                 continue
 
             prevFile = currPrevDic[currFile]
@@ -48,12 +67,13 @@ class CullPictures(object):
             currImg = cv2.imread(self.imageDir + currFile)
             currImgNum = self.extractNum(currFile)
 
-            cv2.putText(currImg, str(currImgNum), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 0))
-            cv2.putText(prevImg, str(prevImgNum), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 0))
             if (prevFile is None):
                 diffImg = np.zeros((500, 500, 3), np.uint8) + 128
             else:
                 diffImg = cv2.absdiff(currImg, prevImg)
+
+            cv2.putText(currImg, str(currImgNum), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 0))
+            cv2.putText(prevImg, str(prevImgNum), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 0))
 
             cv2.imshow("current", currImg)
             cv2.imshow("previous", prevImg)
@@ -62,18 +82,18 @@ class CullPictures(object):
             x = cv2.waitKey(0)
             ch = chr(x & 0xFF)
 
-            if (ch == 'q'):
+            if (ch == 'q'):     # quit
                 break
-            elif (ch == 'd'):
+            elif (ch == 'd'):   # delete
                 self.toBeDeleted.append(currFile)
                 i += 1
                 if (i < len(self.filenames)):
                     currPrevDic[self.filenames[i]] = currPrevDic[currFile]
-            elif (ch == 'b'):
+            elif (ch == 'b'):   # "undo"
                 i -= 1
-                if (currFile in self.toBeDeleted):
-                    self.toBeDeleted.remove(currFile)
-            else:  # keep the image
+                if (self.filenames[i] in self.toBeDeleted):
+                    self.toBeDeleted.remove(self.filenames[i])
+            else:               # keep the image
                 i += 1
                 if (i < len(self.filenames)):
                     currPrevDic[self.filenames[i]] = currFile
@@ -82,6 +102,10 @@ class CullPictures(object):
 
 
     def writeData(self):
+        """
+        Writes the file names of to-be-deleted frames one file name per line into a txt file (self.outputFileName). If
+        the output file does not exist, makes one. If it does, adds to the existing file.
+        """
         matchFile = open(self.outputFileName, 'a')
         matchFile.write("--------------------\n")
         for item in self.toBeDeleted:
@@ -90,7 +114,7 @@ class CullPictures(object):
 
 
     def extractNum(self, fileString):
-        """finds sequence of digits"""
+        """Finds sequence of digits"""
         numStr = ""
         foundDigits = False
         for c in fileString:
@@ -106,8 +130,8 @@ class CullPictures(object):
 
 
 if __name__ == "__main__":
-    cullPicture = CullPictures(imageDir="/home/macalester/turtlebot_videos/atriumClockwiseFrames/",
-                               outputFileName="/home/macalester/turtlebot_videos/atriumClockwiseDelete.txt",
+    cullPicture = CullPictures(imageDir="/Users/JJ/Desktop/atriumSouth/atriumSouthFrames_2/",
+                               outputFileName="/Users/JJ/Desktop/atriumSouth/atriumSouthDelete_2.txt",
                                startFileName="")
 
     cullPicture.go()
