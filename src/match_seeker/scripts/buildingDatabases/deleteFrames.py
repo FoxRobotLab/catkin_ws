aa3092d77edaa0b543fdfa07b83561c5fa17cb7e
susaneileenfox <fox@macalester.edu>
2018-05-31 11:48:28 -0500

    Minor changes in documentation and debug printouts.

--- src/match_seeker/scripts/buildingDatabases/deleteFrames.py
+++ src/match_seeker/scripts/buildingDatabases/deleteFrames.py
@@ -1,9 +1,8 @@
 import os
-import cv2
-import numpy as np
 
 """ ------------------------------------------------------------------------------------------------------------------
 File: deleteFrames.py
+Authors: Jane Pellegrini, Jinyoung Lim
 Date: May 2018
 
 Simply goes through a text file that is created by cullPictures and has names of frames to be removed line by line to 
@@ -27,17 +26,19 @@ class DeleteFrames(object):
 
     def deleteFrames(self):
         self.readDeleteTextLines()
+        counter = 0
         for frameName in self.toBeDeleted:
             filePath = self.imageDirPath + frameName
             # Check if the file exists: https://stackoverflow.com/questions/8933237/how-to-find-if-directory-exists-in-python
             if (os.path.exists(filePath)):
                 os.remove(self.imageDirPath + frameName)
+                counter += 1
             else:
                 print("Path " + filePath + " does NOT exist!")
 
-        print("Deleted from " + self.imageDirPath + " total of " + str(len(self.toBeDeleted())))
+        print("Deleted from " + self.imageDirPath + " total of " + str(counter))
 
 if __name__ == "__main__":
-    deleter = DeleteFrames(imageDirPath="/home/macalester/turtlebot_videos/atriumSouthFrames_2 (copy)/",    #do NOT forget to put "/" at the end!
-                           deleteTextFileName="/home/macalester/turtlebot_videos/atriumSouthDelete_2 (copy).txt")
+    deleter = DeleteFrames(imageDirPath="/home/macalester/turtlebot_videos/atriumSouthFrames_2/",    #do NOT forget to put "/" at the end!
+                           deleteTextFileName="/home/macalester/turtlebot_videos/atriumSouthDelete_2.txt")
     deleter.deleteFrames()
