953ba482ef850f94d07d7a02174ff78ef7b92b05
susaneileenfox <fox@macalester.edu>
2018-06-05 16:58:17 -0500

    Minor modification to run matchplanner

--- src/match_seeker/scripts/DataPaths.py
+++ src/match_seeker/scripts/DataPaths.py
@@ -22,15 +22,16 @@ basePath = "/home/macalester/catkin_ws/src/match_seeker/"
 # basePath = "/home/macalester/Desktop/githubRepositories/catkin_ws/src/match_seeker/"
 
 #
-# turtleBase = os.environ["TURTLEBOT_BASE"]
-# #
-# if turtleBase == "create":
-#     imageDirectory = "res/create060717/"
-#     locData = "res/locations/SUSANcreate0607.txt"
-# elif turtleBase == "kobuki":
-#     imageDirectory = "res/kobuki0615/"
-#     locData = "res/locations/Susankobuki0615.txt"
-
+turtleBase = os.environ["TURTLEBOT_BASE"]
+#
+if turtleBase == "create":
+    imageDirectory = "res/create060717/"
+    locData = "res/locations/SUSANcreate0607.txt"
+elif turtleBase == "kobuki":
+    # imageDirectory = "res/kobuki0615/"
+    imageDirectory = "res/allFrames060418/"
+    # locData = "res/locations/Susankobuki0615.txt"
+    locData = "res/locdata/allLocs060418.txt"
 
 graphMapData = "res/map/olinGraph.txt"
 mapLineData = "res/map/olinNewMap.txt"
