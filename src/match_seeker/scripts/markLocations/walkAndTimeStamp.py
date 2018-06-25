9b4e0eda105fa2638f8bf1bcf6dd0da22e14c621
malisharm <cheRRy2008>
2018-06-25 14:41:47 -0500

    making sure all frames are on github

--- src/match_seeker/scripts/markLocations/walkAndTimeStamp.py
+++ src/match_seeker/scripts/markLocations/walkAndTimeStamp.py
@@ -1,49 +1,246 @@
+# coding=utf-8
+"""--------------------------------------------------------------------------------------------------------------------
+This program was built off the bones of the locsForFrames program. This program allows a user to follow the robot and mark
+the robot's location in real time. The click number, time when the "next" button is pressed, x-coordinate, y-coordinate,
+and heading are all read into a text file. This program features two GUIs: one is a map the user can click on in order
+to record location, and the other is a GUI that shows the x-coordinate, y-coordinate, heading, and the number click the
+user is on.
+
+Author: Malini Sharma
+--------------------------------------------------------------------------------------------------------------------"""
+
 import os
 import time
-import turtleControl
 
 import cv2
 import numpy as np
-import locsForFrames
 
 # import readMap
 import src.match_seeker.scripts.markLocations.readMap as readMap
 
 
+class RealTimeLocs(object):
 
-#image, _ = self.robot.getImage()
-#self.robot.stop()
-
-class LabeledFramesByMeter(object):
-    def __init__(self, outputFilePath, mapFile, robot):
-
-        self.outputFilePath = outputFilePath
-        self.mapFile = mapFile
-        self.robot = robot
-
+    def __init__(self, mapFile, outputFilePath):
+        """Set up data to be held, including displayed and stored maps, current labeling location
+        and heading, and dictionary mapping click numbers to times, locations, and headings."""
         self.currLoc = (0, 0)
         self.currHeading = 0
-        self.imgFileList = []
+
+        # variables for managing data source
+        self.clickNum = 1
+        self.mapFilename = mapFile
+        self.outputFilePath = outputFilePath
 
         # instance variables to hold displayed images
-        self.mainImg = None
+        self.locGui = None
         self.origMap = None
         self.currMap = None
         self.currFrame = None
+        self.currTime = 0
 
         # Instance variables to hold outcome data
-        self.labeling = dict()
+        self.info = dict()
 
+
+    def go(self):
+        """Run the program, setting up windows and all."""
+        self._setupWindows()
+
+        # set up gui window and callback
+        self.locGui = self._buildMainImage()
+        self._displayStatus()
+        cv2.imshow("Main", self.locGui)
+        cv2.setMouseCallback("Main", self._mouseMainResponse)   # Set heading
+
+        # set up map window and callback
+        self.origMap = self._getOlinMap()
+        (self.mapHgt, self.mapWid, dep) = self.origMap.shape
+        self.currMap = self.origMap
+        cv2.imshow("Map", self.currMap)
+        cv2.setMouseCallback("Map", self._mouseSetLoc)          # Set x, y location
+
+        # run main loop until user quits or run out of frames
+        ch = ' '
+        while ch != 'q':
+            x = cv2.waitKey(20)
+            ch = chr(x & 0xFF)
+            if self.currLoc != (0, 0): # if robot location has been placed on the map
+                (mapX, mapY) = self._convertWorldToMap(self.currLoc[0], self.currLoc[1])
+                if ch in "wasd":
+                    if ch == 'a':
+                        mapX -= 2
+                    elif ch == 'd':
+                        mapX += 2
+                    elif ch == 'w':
+                        mapY -= 2
+                    elif ch == 's':
+                        mapY += 2
+                    self.currLoc = self._convertMapToWorld(mapX, mapY)
+                    self._updateMap( (mapX, mapY) )
+
+        # close things down nicely
+        self._writeData()
+        cv2.destroyAllWindows()
+
+
+    def _writeData(self):
+        """Write the data collected to a timestamped file."""
+        try:
+            os.makedirs(self.outputFilePath)
+        except:
+            pass
+        logName = time.strftime("Data-%b%d%a-%H:%M:%S.txt")
+        print(logName)
+        fileOpen = False
+        logFile = None
+        try:
+            logFile = open(self.outputFilePath + logName, 'w')
+            fileOpen = True
+        except:
+            print ("FAILED TO OPEN DATA FILE")
+
+        for clickNum in self.info:
+            [curTime, x, y, h] = self.info[clickNum]
+            dataStr = str(clickNum) + " " + str(curTime) + " " +str(x) + " " + str(y) + " " + str(h) + "\n"
+            if fileOpen:
+                logFile.write(dataStr)
+            print("Frame", clickNum, "with location", (x, y, h))
+        logFile.close()
 
 
     def _setupWindows(self):
-        "Creates three windows and moves them to the right places on screen."
+        "Creates two windows and moves them to the right places on screen."
         cv2.namedWindow("Main")
-        cv2.namedWindow("Image")
         cv2.namedWindow("Map")
         cv2.moveWindow("Main", 30, 50)
-        cv2.moveWindow("Image", 30, 550)
         cv2.moveWindow("Map", 700, 50)
+
+
+    def _buildMainImage(self):
+        """Builds the gui window image with buttons at given locations. Note that space is left for
+        displaying the robot's current location, but that is done by a separate method."""
+
+        rose = (204, 204, 255)
+        violet = (255, 204, 229)
+        green = (0, 204, 0)
+
+        newMain = np.zeros((420, 210, 3), np.uint8)
+
+        # Put squares for NE, SE, SW, NW
+        info = [[0, 0, "NW 45"], [0, 140, "SW 135"], [140, 0, "NE 315"], [140, 140, "SE 225"]]
+        for square in info:
+            (x, y, strng) = square
+            cv2.rectangle(newMain, (x, y), (x + 69, y + 69), rose, -1)
+            cv2.putText(newMain, strng, (x + 20, y + 40), cv2.FONT_HERSHEY_PLAIN, 0.8, (0, 0, 0))
+
+        # Put squares for N, S, E, W
+        info = [[70, 0, "N 0"], [70, 140, "S 180"], [0, 70, "W 90"], [140, 70, "E 270"]]
+        for square in info:
+            (x, y, strng) = square
+            cv2.rectangle(newMain, (x, y), (x + 69, y + 69), violet, -1)
+            cv2.putText(newMain, strng, (x + 20, y + 40), cv2.FONT_HERSHEY_PLAIN, 0.8, (0, 0, 0))
+
+        cv2.rectangle(newMain, (62, 350), (141, 400), green, -1)
+        cv2.putText(newMain, "Next", (85, 380), cv2.FONT_HERSHEY_PLAIN, 0.8, (0, 0, 0))
+        return newMain
+
+
+    def _displayStatus(self):
+        """Displays the current location and saved loc counter on the main window."""
+
+        white = (255, 255, 255)
+        cv2.rectangle(self.locGui, (0, 210), (210, 340), (0, 0, 0), -1)
+        floatTemplate = "{0:s} = {1:^6.2f}"
+        intTemplate = "{0:s} = {1:^3d}"
+        xStr = floatTemplate.format('x', self.currLoc[0])
+        yStr = floatTemplate.format('y', self.currLoc[1])
+        hStr = intTemplate.format('yaw', self.currHeading)
+
+        countStr = intTemplate.format('saved loc', self.clickNum)
+        cv2.putText(self.locGui, "Current location:", (20, 240), cv2.FONT_HERSHEY_PLAIN, 1.0, white)
+        cv2.putText(self.locGui, xStr, (30, 260), cv2.FONT_HERSHEY_PLAIN, 1.0, white)
+        cv2.putText(self.locGui, yStr, (30, 280), cv2.FONT_HERSHEY_PLAIN, 1.0, white)
+        cv2.putText(self.locGui, hStr, (30, 300), cv2.FONT_HERSHEY_PLAIN, 1.0, white)
+        cv2.putText(self.locGui, countStr, (20, 330), cv2.FONT_HERSHEY_PLAIN, 1.0, white)
+        cv2.imshow("Main", self.locGui)
+
+
+    def _mouseMainResponse(self, event, x, y, flags, param):
+        """A mouse callback function that reads the user's click and responds by updating the robot's heading,
+        or by moving on to the next frame in the video."""
+
+        if event == cv2.EVENT_LBUTTONDOWN:
+            if (0 <= x < 70) and (0 <= y < 70):
+                # click was in "northwest" heading square
+                self.currHeading = 45
+            elif (0 <= x < 70) and (70 <= y < 140):
+                # click was in "west" heading square
+                self.currHeading = 90
+            elif (0 <= x < 70) and (140 <= y < 210):
+                # click was in "southwest" heading square
+                self.currHeading = 135
+            elif (70 <= x < 140) and (0 <= y < 70):
+                # click was in "north" heading square
+                self.currHeading = 0
+            elif (70 <= x < 140) and (140 <= y < 210):
+                # click was in "south" heading square
+                self.currHeading = 180
+            elif (140 <= x < 210) and (0 <= y < 70):
+                # click was in "northeast" heading square
+                self.currHeading = 315
+            elif (140 <= x < 210) and (70 <= y < 140):
+                # click was in "east" heading square
+                self.currHeading = 270
+            elif (140 <= x < 210) and (140 <= y < 210):
+                # click was in "southeast" heading square
+                self.currHeading = 225
+            elif (62 <= x < 141) and (350 <= y <= 400):
+                # click was in "Next" frame
+                time1 = time.localtime()
+                self.currTime = time.strftime("%H:%M:%S", time1)
+                self.info[self.clickNum] = [self.currTime, self.currLoc[0], self.currLoc[1], self.currHeading]
+                self.clickNum = self.clickNum + 1
+
+            self._displayStatus()
+
+    def _getOlinMap(self):
+        """Read in the Olin Map and return it. Note: this has hard-coded the orientation flip of the particular
+        Olin map we have, which might not be great, but I don't feel like making it more general. Future improvement
+        perhaps."""
+        origMap = readMap.createMapImage(self.mapFilename, 20)
+        map2 = np.flipud(origMap)
+        olinMap = np.rot90(map2)
+        return olinMap
+
+
+    def _mouseSetLoc(self, event, x, y, flags, param):
+        """A mouse callback function that reads the user's click location and marks it on the map, also making it
+        available to the program to use."""
+
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.currLoc = self._convertMapToWorld(x, y)
+            print "mouseSetLoc self.currLoc =", self.currLoc
+            self._updateMap( (x, y) )
+
+
+    def _updateMap(self, mapLoc):
+        self.currMap = self._highlightLocation(mapLoc, "pixels")
+        cv2.imshow("Map", self.currMap)
+        self._displayStatus()
+
+
+    def _highlightLocation(self, currPos, units = "meters"):
+        """Hightlight the robot's current location based on the input.
+        currPos is given in meters, so need to convert: 20 pixels per meter."""
+        newMap = self.origMap.copy()
+        if units == "meters":
+            (currX, currY) = currPos
+            (mapX, mapY) = self._convertWorldToMap(currX, currY)
+        else:
+            (mapX, mapY) = currPos
+        cv2.circle(newMap, (mapX, mapY), 6, (0, 0, 255))
+        return newMap
 
 
     def _convertMapToWorld(self, mapX, mapY):
@@ -69,19 +266,12 @@ class LabeledFramesByMeter(object):
         mapY = self.mapHgt - 1 - pixelX
         return (int(mapX), int(mapY))
 
+if __name__ == "__main__":
+    catkinPath = "/home/macalester/"
+    # catkinPath = "/Users/susan/Desktop/ResearchStuff/Summer2016-2017/GithubRepositories/"
+    basePath = "PycharmProjects/catkin_ws/src/match_seeker/"
 
+    realTimer = RealTimeLocs(mapFile=catkinPath + basePath + "res/map/olinNewMap.txt",
+                             outputFilePath= "/home/macalester/testframes/")
 
-
-
-if __name__ == "main":
-    catkinPath = "/Users/johnpellegrini/"
-    basePath= "PycharmProjects/catkin_ws/src/match_seeker/"
-
-    robot = turtleControl.TurtleBot()
-
-
-    liveAssigningLocations = LabeledFramesByMeter(mapFile=catkinPath + basePath + "res/map/olinNewMap.txt",
-                                                  outputFilePath= "/Users/johnpellegrini/Desktop/recordedVideos/",
-                                                  robot = robot)
-    liveAssigningLocations._setupWindows()
-
+    realTimer.go()
