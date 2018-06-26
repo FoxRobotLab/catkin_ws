13132d09d27d3162dd29799681f9df7a943b1c92
jpellegr <jpellegr@macalester.edu>
2018-06-26 14:44:46 -0500

    almost finished with matchForCheckPoints

--- src/match_seeker/scripts/markLocations/matchForCheckPoints.py
+++ src/match_seeker/scripts/markLocations/matchForCheckPoints.py
@@ -1,14 +1,96 @@
 import os
 import cv2
-import rospy
 import src.match_seeker.scripts.markLocations.readMap as readMap
 import numpy as np
+import time
+import math
 
 class Matcher(object):
 
-    def __init__(self, walkerTextFile, autoTextFile):
+    def __init__(self, walkerTextFile, autoTextFile, outputTextFile):
         self.walkerTextFile = walkerTextFile
         self.autoTextFile = autoTextFile
+        self.outputTextFile = outputTextFile
+
+
+    def findMatches(self):
+        walker, walkerTimes = self.prepareWalker()
+        auto, autoTimes = self.prepareAuto()
+
+        output = open(self.outputTextFile, "w")
+        for line in walkerTimes:
+            autoIndex = self.compareWithAllAutoTimes(line, autoTimes)
+
+        output.close()
+
+
+    def prepareWalker(self):
+        """
+        Reads the text file of all of the frames saved by the person walking, reads them. Returns two lists, the first
+        containing all of the times, the second containing all of the data.
+
+        :return:
+        """
+        timeList = []
+        walker = open(self.walkerTextFile, "r")
+        walker = walker.readlines()
+        for line in walker:
+            elems= line.split()
+            timeList.append(self.convertTimeToSeconds(elems[1]))
+        return timeList, walker
+
+    def prepareAuto(self):
+        """
+        Reads the text file of all of frames saved by the computer, reads them. Returns two lists, the first containing
+        all of the times, the second containing all of the data.
+
+        :return:
+        """
+        timeList = []
+        auto = open(self.autoTextFile, "r")
+        auto = auto.readlines()
+        for line in auto:
+            elems = line.split()
+            timeList.append(self.convertTimeToSeconds(elems[1]))
+        return timeList, auto
+
+
+
+    def compareWithAllAutoTimes(self, walkerTime, autoTimes):
+        """
+        Finds the index in the auto list which is the most similar to the given walkerTime
+
+        :param walkerTime:
+        :param autoTimes:
+        :return:
+        """
+        minDif = np.inf
+        difIndex =0
+        for i in range(len(autoTimes)):
+            autoTime = autoTimes[i]
+            diff = abs(autoTime - walkerTime)
+            if diff < minDif:
+                minDif = diff
+                difIndex =i
+        return difIndex
+
+
+
+
+
+
+    def convertTimeToSeconds(self, timeString):
+        """
+        Takes in a string of time in the format HR:MN:S and returns the total number of seconds
+
+        :param timeString:
+        :return:
+        """
+        timeList= timeString.split(":")
+        secondsFromHours = int(timeList[0]) * 60 * 60
+        secondsFromMinutes = int(timeList[1]) * 60
+        totalTime = secondsFromHours + secondsFromMinutes + int(timeList[2])
+        return(totalTime)
 
 
 
@@ -19,8 +101,8 @@ class Matcher(object):
 
 
 
-
-
-if __name__ == "main":
+if __name__ == "__main__":
     matchWalkerAndImages = Matcher(walkerTextFile= "",
-                                   autoTextFile = "")
+                                   autoTextFile = "",
+                                   outputTextFile = "")
+
