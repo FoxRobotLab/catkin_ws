f2417cb6b87ac3bb747341d67c6105d0d569390f
Susan Eileen Fox <fox@macalester.edu>
2020-06-14 14:30:14 -0500

    Delete paths.py

--- src/match_seeker/scripts/olri_classifier/paths.py
+++
@@ -1,3 +0,0 @@
-
-pathToMatchSeeker = '/home/macalester/PycharmProjects/catkin_ws/src/match_seeker/'
-pathToClassifier2019 = '/home/macalester/PycharmProjects/catkin_ws/src/match_seeker/res/classifier2019data/'
