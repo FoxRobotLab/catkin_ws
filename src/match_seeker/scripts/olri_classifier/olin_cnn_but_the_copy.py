ff15512777c39749616b7d0ae00b67b4f1746aff
arifzamil <azamil@macalester.edu>
2020-06-15 12:49:27 -0700

    stuff

--- src/match_seeker/scripts/olri_classifier/olin_cnn_but_the_copy.py
+++ src/match_seeker/scripts/olri_classifier/olin_cnn_but_the_copy.py
@@ -605,37 +605,27 @@ def clean_image(image, data = 'old', cell = None, heading = None):
 if __name__ == "__main__":
     # check_data()
     olin_classifier = OlinClassifier(
-        dataImg= pathToMatchSeeker+ 'res/classifier2019data/SAMPLETRAININGDATA_IMG_withCellInput135K.npy',
-        dataLabel = pathToMatchSeeker+ 'res/classifier2019data/SAMPLETRAININGDATA_HEADING_withCellInput135K.npy',
+        dataImg= pathToMatchSeeker+ 'res/classifier2019data/TRAININGDATA_IMG_withHeadingInput135K.npy',
+        dataLabel = pathToMatchSeeker+ 'res/classifier2019data/TRAININGDATA_CELL_withHeadingInput135K.np',
         data_name = "cell",
-        outputSize= 8,
+        outputSize= 271,
         eval_ratio=0.1,
         image_size=100,
-        cellInput= True,
+        headingInput= True,
         image_depth= 2
     )
-    # print("Classifier built")
-    # olin_classifier.loadData()
-    # print("Data loaded")
-    # olin_classifier.train()
+
+    # count = 0
+    # for i in range(10):
+    #     num = random.randint(0,95000)
+    #     thing, heading = olin_classifier.runSingleImage(num, input='cell')
+    #     print(np.argmax(thing), " thing", thing)
+    #     print(heading, " heading")
 
 
-
-
-    # print(len(olin_classifier.train_images))
-    #olin_classifier.train()
-    # olin_classifier.getAccuracy()
     count = 0
     for i in range(10):
-        num = random.randint(0,95000)
-        thing, heading = olin_classifier.runSingleImage(num, input='cell')
+        num = random.randint(0, 95000)
+        thing, cell = olin_classifier.runSingleImage(num)
         print(np.argmax(thing), " thing", thing)
-        print(heading, " heading")
-
-
-    # model = olin_classifier.threeConv()
-    # olin_classifier.train()
-    #
-    # self.cell_model = keras.models.load_model(
-    #     "/home/macalester/PycharmProjects/catkin_ws/src/match_seeker/scripts/olri_classifier/CHECKPOINTS/cell_acc9705_headingInput_155epochs_95k_NEW.hdf5",
-    #     compile=True)
+        print(cell, " cell")
