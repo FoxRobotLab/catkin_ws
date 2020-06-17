2464fcba7d1b59d6f3d81714e78a48f7d4337dd1
Jinyoung Lim <jlim2@macalester.edu>
2020-06-17 17:37:59 -0500

    updated

--- src/match_seeker/scripts/olri_classifier/findingTheCorner.py
+++ src/match_seeker/scripts/olri_classifier/findingTheCorner.py
@@ -1,11 +1,12 @@
 import numpy as np
 from paths import frames
+from collections import OrderedDict
 
 master_cell_loc_frame_id = frames + '/MASTER_CELL_LOC_FRAME_IDENTIFIER.txt'
 
 
 def getCorner():
-    corner = dict()
+    corner = OrderedDict()
     numFormat = "{0:0>4d}"
     pastFrame = []
     with open(master_cell_loc_frame_id, 'r') as frameList:
@@ -21,28 +22,21 @@ def getCorner():
                 pastFrame.pop(0)
             currentCell = split[1]
             if currentCell!= beforeCell:
-                print(currentCell)
-                print(currentCell)
-                print(cornerNum)
-                print('#')
+                f5 = lines[lineNum+1].split()[0]
+                f5 = numFormat.format(int(f5))
+                f6 = lines[lineNum+2].split()[0]
+                f6 = numFormat.format(int(f6))
+                pastFrame.append(f5)
+                pastFrame.append(f6)
+                corner['c'+ str(cornerNum)] = pastFrame.copy()
+                pastFrame.pop(0)
+                pastFrame.pop(0)
                 cornerNum+=1
-                return 0
-            #if currentCell != beforeCell:
-                #f5 = lines[lineNum+1].split()[0]
-                #f5 = numFormat.format(int(f5))
-                #f6 = lines[lineNum+2].split()[0]
-                #f6 = numFormat.format(int(f6))
-                #pastFrame.append(f5)
-                #pastFrame.append(f6)
-                #corner['c'+ str(cornerNum)] = pastFrame
-                #pastFrame.pop(0)
-                #pastFrame.pop(0)
-                #cornerNum+=1
-            print(pastFrame)
             beforeCell = currentCell
             lineNum += 1
+    return corner
 
 
 
 if __name__ == '__main__':
-    getCorner()
+    print(getCorner())
