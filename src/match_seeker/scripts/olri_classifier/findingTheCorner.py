3187309106ca28ce0edbe0ad33386dbb90c592a7
Jinyoung Lim <jlim2@macalester.edu>
2020-06-17 16:16:11 -0500

    finding the corners

--- src/match_seeker/scripts/olri_classifier/findingTheCorner.py
+++ src/match_seeker/scripts/olri_classifier/findingTheCorner.py
@@ -1,20 +1,48 @@
 import numpy as np
-master_cell_loc_frame_id = DATA + 'frames/MASTER_CELL_LOC_FRAME_IDENTIFIER.txt'
+from paths import frames
+
+master_cell_loc_frame_id = frames + '/MASTER_CELL_LOC_FRAME_IDENTIFIER.txt'
+
 
 def getCorner():
-    corner = {}
+    corner = dict()
     numFormat = "{0:0>4d}"
     pastFrame = []
     with open(master_cell_loc_frame_id, 'r') as frameList:
         lines = frameList.readlines()
-        print("cell?", lines[0][1])
-        return 0
+        beforeCell = lines[0].split()[1]
+        lineNum = 0
+        cornerNum=1
         for line in lines:
-            splitLine = line.split()
-            frame = numFormat.format(splitLine[0])
+            split = line.split()
+            frame = numFormat.format(int(split[0]))
             pastFrame.append(frame)
-            if len(pastFrame)> 3:
+            if len(pastFrame)> 4:
                 pastFrame.pop(0)
-            print("This is the pastFrame", pastFrame)
+            currentCell = split[1]
+            if currentCell!= beforeCell:
+                print(currentCell)
+                print(currentCell)
+                print(#)
+                print(cornerNum)
+                cornerNum +=1
+                break 
+            #if currentCell != beforeCell:
+                #f5 = lines[lineNum+1].split()[0]
+                #f5 = numFormat.format(int(f5))
+                #f6 = lines[lineNum+2].split()[0]
+                #f6 = numFormat.format(int(f6))
+                #pastFrame.append(f5)
+                #pastFrame.append(f6)
+                #corner['c'+ str(cornerNum)] = pastFrame
+                #pastFrame.pop(0)
+                #pastFrame.pop(0)
+                #cornerNum+=1
+            print(pastFrame)
+            beforeCell = currentCell
+            lineNum += 1
+      
 
 
+if __name__ == '__main__':
+    getCorner()
