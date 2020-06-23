a477b9177474b2897ab8dcd96340d194c0929637
analeidibarrera <abarrera@macalester.edu>
2020-06-23 15:43:34 -0500

    test

--- src/match_seeker/scripts/olri_classifier/olin_cnn.py
+++ src/match_seeker/scripts/olri_classifier/olin_cnn.py
@@ -93,7 +93,7 @@ class OlinClassifier(object):
                 compile=True)
         elif self.cellInput:
             #self.model = self.cnn_cells()
-            self.model = cnn_cells
+            self.model = cnn_cells.cnn_cells(self)
             self.loss = keras.losses.categorical_crossentropy
         else:  # both as input, seems weird
             print("At most one of cellInput and headingInput should be true.")
