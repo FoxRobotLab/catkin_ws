52a2949d81054cef6769a1a2207cd84e24251120
analeidibarrera <abarrera@macalester.edu>
2020-06-25 10:38:28 -0500

    update

--- src/match_seeker/scripts/olri_classifier/olin_cnn.py
+++ src/match_seeker/scripts/olri_classifier/olin_cnn.py
@@ -173,7 +173,7 @@ class OlinClassifier(object):
         #         metrics=["accuracy"]
         #     )
         #self.model.build(self.train_images.shape)
-        self.model.build(self.train_images.shape)
+        self.model.build()
         self.model.summary()
         return 0 #!!!!!!!!!CHANGE THIS BACK!!!!!!!!!!
 
