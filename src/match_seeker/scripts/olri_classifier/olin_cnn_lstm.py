50c722ba6c4f37c4a2b8986c7ca38b14442f8d2a
Jinyoung Lim <jlim2@macalester.edu>
2020-06-25 10:14:47 -0500

    merge

--- src/match_seeker/scripts/olri_classifier/olin_cnn_lstm.py
+++ src/match_seeker/scripts/olri_classifier/olin_cnn_lstm.py
@@ -4,6 +4,9 @@ Tx = 30
 n_x = 3
 n_s = 64
 X = keras.layers.Input(shape=(Tx, n_x))
+print("This is the X", X)
+print("This is the shape", X.shape)
 s, a, c = keras.layers.LSTM(n_s, return_sequences=True, return_state=True)(X)
+print("This is s", s, "This is a ", a, "This is c", c)
 model_LSTM = keras.Model(inputs=X, outputs=[s, a, c])
 model_LSTM.summary()
