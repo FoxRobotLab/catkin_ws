e50d956cc60afa02a90dca8f01ec3348d510ff55
analeidibarrera <abarrera@macalester.edu>
2020-06-25 10:32:36 -0500

    update

--- src/match_seeker/scripts/olri_classifier/olin_cnn_lstm.py
+++ src/match_seeker/scripts/olri_classifier/olin_cnn_lstm.py
@@ -2,7 +2,7 @@ from tensorflow import keras
 def cnn_cells(self):
     print("Building a model that takes cell number as input")
     cnn = keras.models.Sequential()
-    cnn.add(keras.layers.Conv2D(
+    cnn.add(keras.layers.TimeDistributed(keras.layers.Conv2D(
         filters=128,
         kernel_size=(5, 5),
         strides=(1, 1),
@@ -11,13 +11,13 @@ def cnn_cells(self):
         data_format="channels_last",
         input_shape=[self.image_size, self.image_size, self.image_depth]
 
-    ))
-    cnn.add(keras.layers.MaxPooling2D(
+    )))
+    cnn.add(keras.layers.TimeDistributed(keras.layers.MaxPooling2D(
         pool_size=(2, 2),
         strides=(2, 2),
         padding="same"
-    ))
-    cnn.add(keras.layers.Flatten())
+    )))
+    cnn.add(keras.layers.TimeDistributed(keras.layers.Flatten()))
     print("This is what it was before", cnn)
     return cnn
 
