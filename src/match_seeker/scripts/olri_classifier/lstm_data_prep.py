281ced7d5f2ee2201bdc6f17765014138a94f1c5
analeidibarrera <abarrera@macalester.edu>
2020-06-29 12:40:15 -0500

    Data loaded properly

--- src/match_seeker/scripts/olri_classifier/lstm_data_prep.py
+++ src/match_seeker/scripts/olri_classifier/lstm_data_prep.py
@@ -140,11 +140,10 @@ if __name__ == '__main__':
     # cell_heading_counts = getHeadingRep(cell_counts)
     # cullOverRepped(cell_counts, cell_frame_dict, cell_heading_counts)
     # addUnderRepped(cell_counts, cell_frame_dict, cell_heading_counts)
-    cell_frame_dict= open(DATA+ 'cell_origframes_500orL.npy')
-    print(type(cell_frame_dict))
-    # for key in cell_frame_dict.keys():
-    #     print(key, len(cell_frame_dict[key]))
-    # rndUnderRepSubset = np.load(DATA + 'cell_newframes_dict')
-    # for key in rndUnderRepSubset.keys():
-    #     print(key, len(rndUnderRepSubset[key]))
+    cell_frame_dict= open(DATA+ 'cell_origframes_500orL.npy',allow_pickle='TRUE').item()
+    for key in cell_frame_dict.keys():
+        print(key, len(cell_frame_dict[key]))
+    rndUnderRepSubset = np.load(DATA + 'cell_newframes_dict', allow_pickle='TRUE').item()
+    for key in rndUnderRepSubset.keys():
+        print(key, len(rndUnderRepSubset[key]))
 
