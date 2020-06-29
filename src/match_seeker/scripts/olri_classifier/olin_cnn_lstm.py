8fbe4075c417946f3356eb2756a8f4916f6da261
Jinyoung Lim <jlim2@macalester.edu>
2020-06-29 09:22:28 -0500

    old CNN with a new basic LSTM

--- src/match_seeker/scripts/olri_classifier/olin_cnn_lstm.py
+++ src/match_seeker/scripts/olri_classifier/olin_cnn_lstm.py
@@ -18,8 +18,36 @@ def cnn_cells(self):
         strides=(2, 2),
         padding="same"
     )))
+    cnn.add(keras.layers.TimeDistributed(keras.layers.Dropout(0.4)))
+    cnn.add(keras.layers.TimeDistributed(keras.layers.Conv2D(
+            filters=64,
+            kernel_size=(5, 5),
+            strides=(1, 1),
+            activation="relu",
+            padding="same"
+        )))
+    cnn.add(keras.layers.TimeDistributed(keras.layers.MaxPooling2D(
+            pool_size=(2, 2),
+            strides=(2, 2),
+            padding="same"
+        )))
+    cnn.add(keras.layers.TimeDistributed(keras.layers.Dropout(0.4)))
+    cnn.add(keras.layers.TimeDistributed(keras.layers.Conv2D(
+            filters=64,
+            kernel_size=(5, 5),
+            strides=(1, 1),
+            activation="relu",
+            padding="same",
+            data_format="channels_last"
+        )))
+    cnn.add(keras.layers.TimeDistributed(keras.layers.MaxPooling2D(
+            pool_size=(2, 2),
+            strides=(2, 2),
+            padding="same",
+        )))
+    cnn.add(keras.layers.TimeDistributed(keras.layers.Dropout(0.4)))
     cnn.add(keras.layers.TimeDistributed(keras.layers.Flatten()))
-    cnn.add(keras.layers.LSTM(50))
+    cnn.add(keras.layers.LSTM(10))
     cnn.add(keras.layers.Dense(8, activation='sigmoid'))
     cnn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     cnn.summary()
