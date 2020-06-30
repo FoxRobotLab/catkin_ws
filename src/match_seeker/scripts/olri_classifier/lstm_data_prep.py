97952a38ff48ac6279f45a9de1255b001350469f
analeidibarrera <abarrera@macalester.edu>
2020-06-30 10:18:14 -0500

    taking out the print

--- src/match_seeker/scripts/olri_classifier/lstm_data_prep.py
+++ src/match_seeker/scripts/olri_classifier/lstm_data_prep.py
@@ -232,7 +232,6 @@ def add_cell_channel(cell_frame_dict = None, rndUnderRepSubset = None , cellInpu
         for frame in cell_frame_dict[key]:
             cell_frame_dict[key][whichFrame] = (frame, notNewImages[key][whichFrame])
             whichFrame += 1
-            print(cell_frame_dict[key])
         cell_frame_dict[key] = sorted(cell_frame_dict[key])
 
     print("This is the dictionar with tuples", cell_frame_dict)
