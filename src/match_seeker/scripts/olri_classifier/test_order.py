2c21d6d60abe62dfab51e0e10f52c1baeff12cd8
analeidibarrera <abarrera@macalester.edu>
2020-07-06 13:34:17 -0500

    test

--- src/match_seeker/scripts/olri_classifier/test_order.py
+++ src/match_seeker/scripts/olri_classifier/test_order.py
@@ -12,7 +12,7 @@ cnt = 0
 while (cnt != 12001):
     img = image[cnt]
     font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(np.float32(img), str(cnt), (10,10), font, 1, (0, 255, 0), 1)
+    img= cv2.putText(np.float32(img), str(cnt), (10,10), font, 10, (0, 255, 0), 1)
     cv2.imshow("Window", img)
     cv2.waitKey(0)
     cnt +=1
