3417b6f8a9ed8b18727f509769821da9a6ba7b16
analeidibarrera <abarrera@macalester.edu>
2020-07-06 13:13:25 -0500

    test

--- src/match_seeker/scripts/olri_classifier/test_order.py
+++ src/match_seeker/scripts/olri_classifier/test_order.py
@@ -13,14 +13,13 @@ while (cnt != 12001):
     img = image[cnt]
     font = cv2.FONT_HERSHEY_SIMPLEX
     #img_text = cv2.putText(img, str(cnt), (10,10), font, 50, (0, 255, 0), 1, 1)
-    while(True):
-        cv2.imshow("Window", img)
-        x = cv2.waitKey(10)
-        userChar = chr(x & 0xFF)
-        if userChar == 'r':
-            cnt -=1
-            img = image[cnt]
-        elif userChar =='n':
-            break
+    cv2.imshow("Window", img)
+    x = cv2.waitKey(30)
+    userChar = chr(x & 0xFF)
+    if userChar == 'r':
+        cnt -=1
+        img = image[cnt]
+    elif userChar =='n':
+        break
     cnt +=1
-    cv2.destroyAllWindows()
+cv2.destroyAllWindows()
