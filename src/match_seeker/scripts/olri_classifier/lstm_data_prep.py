f48a8efb0317720b81ec99f5cff7103839d88631
analeidibarrera <abarrera@macalester.edu>
2020-07-07 07:33:47 -0500

    update

--- src/match_seeker/scripts/olri_classifier/lstm_data_prep.py
+++ src/match_seeker/scripts/olri_classifier/lstm_data_prep.py
@@ -315,17 +315,23 @@ if __name__ == '__main__':
     # addUnderRepped(cell_counts, cell_frame_dict, cell_heading_counts)
 
     cell_frame_dict = np.load(DATA+ 'cell_origFrames.npy',allow_pickle='TRUE').item()
+    rndUnderRepSubset = np.load(DATA + 'cell_newFrames.npy', allow_pickle='TRUE').item()
     wantedCells = ['17', '16', '15', '14', '13', '12', '11', '10', '9', '8', '7', '6', '5', '4', '3', '2', '18', '1',
                    '19','0', '20', '21', '22', '23', '24']
     frame_dict = OrderedDict()
+    newFrames = OrderedDict()
     for cell in wantedCells:
         frame_dict[cell] = cell_frame_dict[cell]
-    print("keys", frame_dict.keys())
-    print("dict", frame_dict)
+        if(rndUnderRepSubset[cell] is not None):
+            newFrames[cell]= rndUnderRepSubset[cell]
+    print(newFrames.keys)
+    print(newFrames)
+        
 
 
 
-    #rndUnderRepSubset = np.load(DATA + 'cell_newFrames.npy', allow_pickle='TRUE').item()
+
+
     #add_cell_channel(cell_frame_dict , rndUnderRepSubset, cellInput= True, headingInput=None)
 
 
