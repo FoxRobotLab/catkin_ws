aaf731faf48fe4dcc3f829bd397e096b6b282830
analeidibarrera <abarrera@macalester.edu>
2020-07-07 17:18:16 -0500

    batch

--- src/match_seeker/scripts/olri_classifier/olin_cnn.py
+++ src/match_seeker/scripts/olri_classifier/olin_cnn.py
@@ -208,7 +208,7 @@ class OlinClassifier(object):
 
         self.model.fit(
             self.train_images, self.train_labels,
-            batch_size= 4,
+            batch_size= 3,
             epochs=6,
             verbose=1,
             validation_data=(self.eval_images, self.eval_labels),
