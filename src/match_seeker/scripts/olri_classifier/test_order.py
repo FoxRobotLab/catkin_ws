daf6192d66fe2e184cdd12421cce8933497b8369
analeidibarrera <abarrera@macalester.edu>
2020-07-07 14:52:55 -0500

    update

--- src/match_seeker/scripts/olri_classifier/test_order.py
+++ src/match_seeker/scripts/olri_classifier/test_order.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     frame_label = getFrameHeadingDict()
 
     ######################The actual hot labels!!!!!!!!!
-    cel = '22'
+    cel = '29'
     start = (int(cel)-18)*500
     hotLabel = []
     for i in range(start, start+500, 1):
