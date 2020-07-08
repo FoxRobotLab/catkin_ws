551a9731c21c8f38b76beb61a4eadcf8910a56b9
analeidibarrera <abarrera@macalester.edu>
2020-07-08 13:43:38 -0500

    generating correct 1000s

--- src/match_seeker/scripts/olri_classifier/lstm_data_prep.py
+++ src/match_seeker/scripts/olri_classifier/lstm_data_prep.py
@@ -269,7 +269,7 @@ def add_cell_channel(cell_frame_dict = None, rndUnderRepSubset = None , cellInpu
 
     #Organizing cells (Specific for sample)
     wantedCells = ['18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34',
-                   '35', '36', '37', '38', '39', '40', '41', '42']
+                   '35', '36', '37', '38', '39', '40', '41', '42', '43']
 
 
     #Creating the array of images and the hot label
