48cccb892b4ff35a6aecae7d587e912486ca580b
analeidibarrera <abarrera@macalester.edu>
2020-07-09 10:36:34 -0500

    different cnn

--- src/match_seeker/scripts/olri_classifier/olin_cnn_lstm.py
+++ src/match_seeker/scripts/olri_classifier/olin_cnn_lstm.py
@@ -1,11 +1,64 @@
 from tensorflow import keras
 import numpy as np
 
+############## HEADING OUTPUT ####################
+# def cnn_cells(self):
+#     print("Building a model that takes cell number as input")
+#     cnn = keras.models.Sequential()
+#     cnn.add(keras.layers.TimeDistributed(keras.layers.Conv2D(
+#         filters= 32,
+#         kernel_size=(5, 5),
+#         strides=(1, 1),
+#         activation="relu",
+#         padding="same",
+#         data_format="channels_last",
+#
+#     ), input_shape= [None, 100, 100, 1]))
+#     cnn.add(keras.layers.TimeDistributed(keras.layers.MaxPooling2D(
+#         pool_size=(2, 2),
+#         strides=(2, 2),
+#         padding="same"
+#     )))
+#     cnn.add(keras.layers.TimeDistributed(keras.layers.Dropout(0.4)))
+#     cnn.add(keras.layers.TimeDistributed(keras.layers.Conv2D(
+#             filters=64,
+#             kernel_size=(5, 5),
+#             strides=(1, 1),
+#             activation="relu",
+#             padding="same"
+#         )))
+#     cnn.add(keras.layers.TimeDistributed(keras.layers.MaxPooling2D(
+#             pool_size=(2, 2),
+#             strides=(2, 2),
+#             padding="same"
+#         )))
+#     cnn.add(keras.layers.TimeDistributed(keras.layers.Dropout(0.4)))
+#     cnn.add(keras.layers.TimeDistributed(keras.layers.Conv2D(
+#             filters=32,
+#             kernel_size=(5, 5),
+#             strides=(1, 1),
+#             activation="relu",
+#             padding="same",
+#             data_format="channels_last"
+#         )))
+#     cnn.add(keras.layers.TimeDistributed(keras.layers.MaxPooling2D(
+#             pool_size=(2, 2),
+#             strides=(2, 2),
+#             padding="same",
+#         )))
+#     cnn.add(keras.layers.TimeDistributed(keras.layers.Dropout(0.4)))
+#     cnn.add(keras.layers.TimeDistributed(keras.layers.Flatten()))
+#     cnn.add(keras.layers.LSTM(10))
+#     cnn.add(keras.layers.Dense(271, activation='sigmoid'))
+#     cnn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+#     cnn.summary()
+#     return cnn
+
 def cnn_cells(self):
     print("Building a model that takes cell number as input")
     cnn = keras.models.Sequential()
     cnn.add(keras.layers.TimeDistributed(keras.layers.Conv2D(
-        filters= 32,
+        filters= 64,
         kernel_size=(5, 5),
         strides=(1, 1),
         activation="relu",
@@ -20,7 +73,7 @@ def cnn_cells(self):
     )))
     cnn.add(keras.layers.TimeDistributed(keras.layers.Dropout(0.4)))
     cnn.add(keras.layers.TimeDistributed(keras.layers.Conv2D(
-            filters=64,
+            filters=32,
             kernel_size=(5, 5),
             strides=(1, 1),
             activation="relu",
@@ -52,7 +105,6 @@ def cnn_cells(self):
     cnn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     cnn.summary()
     return cnn
-
 
 def creatingSequence(data, timeStep, overlap):
     newData = []
