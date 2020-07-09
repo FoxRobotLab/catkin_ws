860ed19189dc9bf2644e9f99cd7265aa9bd85236
arifzamil <azamil@macalester.edu>
2020-07-09 16:31:59 -0700

    stuff

--- src/match_seeker/scripts/olri_classifier/test_order_2.py
+++ src/match_seeker/scripts/olri_classifier/test_order_2.py
@@ -8,10 +8,12 @@ from collections import OrderedDict
 
 data = np.load(DATA + 'regressionTestSet.npz')
 
-xyhOut = data['xyhOut']
-images = data['images']
+# xyhOut = data['xyhOut']
+# images = data['images']
 
 
-np.save(DATA + "regressionImages", xyhOut)
-np.save(DATA + "regressionOutput", images)
+# np.save(DATA + "regressionImages", xyhOut)
+# np.save(DATA + "regressionOutput", images)
 
+images = np.load(DATA + 'regressionImages.npy')
+cv2.imshow("Name", images[0])
