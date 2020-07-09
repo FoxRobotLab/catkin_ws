75df38e59419f460d1d4427e1bc66eb528880536
arifzamil <azamil@macalester.edu>
2020-07-09 16:37:50 -0700

    stuff

--- src/match_seeker/scripts/olri_classifier/test_order_2.py
+++ src/match_seeker/scripts/olri_classifier/test_order_2.py
@@ -16,4 +16,4 @@ data = np.load(DATA + 'regressionTestSet.npz')
 # np.save(DATA + "regressionOutput", images)
 
 images = np.load(DATA + 'regressionImages.npy')
-print(images)
+print(len(data['frames']))
