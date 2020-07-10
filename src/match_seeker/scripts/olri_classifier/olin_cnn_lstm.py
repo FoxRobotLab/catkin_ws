53f8bf874b026c606c8f068c00add62934ee9dd2
analeidibarrera <abarrera@macalester.edu>
2020-07-10 09:00:49 -0500

    test

--- src/match_seeker/scripts/olri_classifier/olin_cnn_lstm.py
+++ src/match_seeker/scripts/olri_classifier/olin_cnn_lstm.py
@@ -61,8 +61,7 @@ def predictingCells(self):
     num_classes = 271
     model = keras.models.load_model(DATA + "CHECKPOINTS/olin_cnn_checkpoint-0708201430/cellInputReference-02-2.00.hdf5")
     new_model = keras.models.Sequential()
-    new_model.add(model(include_top=False,
-                        pooling='avg'))
+    new_model.add(model(include_top=False))
     new_model.add(keras.layers.Dense(num_classes, activation='softmax'))
     new_model.layers[0].trainable = False
     # for i in range(1, 12):
