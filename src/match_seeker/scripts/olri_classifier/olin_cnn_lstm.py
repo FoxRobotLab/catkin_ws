1eedc4c68b13e6fd1ad25c37449e81c830bbdfd8
analeidibarrera <abarrera@macalester.edu>
2020-07-10 13:22:46 -0500

    transfer learning getting rid of the last layer

--- src/match_seeker/scripts/olri_classifier/olin_cnn_lstm.py
+++ src/match_seeker/scripts/olri_classifier/olin_cnn_lstm.py
@@ -61,11 +61,8 @@ def predictingCells(self):
     num_classes = 271
     model = keras.models.load_model(DATA + "CHECKPOINTS/olin_cnn_checkpoint-0708201430/cellInputReference-02-2.00.hdf5")
     new_model = keras.models.Sequential()
-    new_model.add(model(
-                        # include_top=False,
-                        require_flatten= False,
-                        weights = model.load_weights(DATA + "CHECKPOINTS/olin_cnn_checkpoint-0708201430/cellInputReference-02-2.00.hdf5"),
-                        pooling='avg'
+    new_model.add(model(inputs=model.inputs,
+                        outputs=model.layers[-1].output
                          ))
     new_model.add(keras.layers.Dense(num_classes, activation='softmax'))
     new_model.layers[0].trainable = False
