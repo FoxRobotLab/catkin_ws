80ef73c3d1629141303e0fdc719d344b6f2cbf47
analeidibarrera <abarrera@macalester.edu>
2020-07-11 14:38:55 -0500

    test

--- src/match_seeker/scripts/olri_classifier/olin_cnn_lstm.py
+++ src/match_seeker/scripts/olri_classifier/olin_cnn_lstm.py
@@ -60,14 +60,15 @@ def predictingCells(self):
     print("Tinkering with transferLearning")
     num_classes = 271
     new_model = keras.models.load_model(DATA + "CHECKPOINTS/olin_cnn_checkpoint-0708201430/cellInputReference-02-2.00.hdf5")
-    print("This is the number of layes", len(new_model.layers))
     new_model.pop()
-    print("This is the number of layes after pop", len(new_model.layers))
-    #new_model = keras.Model(inputs =new_model.inputs)
+    print("This is the number of layers", len(new_model.layers))
+    new_model.add(keras.layers.Dense(num_classes, activation='sigmoid'))
+    print("This is the number of layers", len(new_model.layers))
+
     #print("This is the number of layes after input and and output", len(new_model.layers))
     # for i in range(1, 11):
     #     new_model.layers[i].trainable = True
-    # new_model.add(keras.layers.Dense(num_classes, activation = 'sigmoid'))
+
     # new_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     # new_model.summary()
     return new_model
