4e5011737b30c586044791c6ddaab7e8b775b8d6
analeidibarrera <abarrera@macalester.edu>
2020-07-11 15:09:23 -0500

    test

--- src/match_seeker/scripts/olri_classifier/olin_cnn_lstm.py
+++ src/match_seeker/scripts/olri_classifier/olin_cnn_lstm.py
@@ -61,12 +61,12 @@ def predictingCells(self):
     num_classes = 271
     new_model = keras.models.load_model(DATA + "CHECKPOINTS/olin_cnn_checkpoint-0708201430/cellInputReference-02-2.00.hdf5")
     new_model.pop()
-    new_model.add(keras.layers.Dense(num_classes, activation='sigmoid'))
+    new_model.add(keras.layers.Dense(num_classes, activation='softmax'))
+    new_model.layers[0].trainable = False
     new_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     new_model.summary()
 
-    #return new_model
-    return 0 
+    return new_model
 
 
 
