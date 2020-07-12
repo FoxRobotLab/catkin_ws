747bc793b3a9c1258436b619af2b41e58e267854
analeidibarrera <abarrera@macalester.edu>
2020-07-12 13:47:01 -0500

    checking that the cells are correctly labeled

--- src/match_seeker/scripts/olri_classifier/test_order.py
+++ src/match_seeker/scripts/olri_classifier/test_order.py
@@ -3,78 +3,21 @@ import numpy as np
 from collections import OrderedDict
 
 
-
+def getOneHotLabel(number,size):
+    onehot = [0] * size
+    onehot[number] = 1
+    return onehot
 
 
 
 if __name__ == '__main__':
-    image_cell = np.load(DATA+ 'lstm_Img_Cell_Input13k.npy')
-    print(image_cell.shape)
-    print(image_cell[0])
-    # cells = []
-    # for cell in image_cell:
-    #     print(cell[1])
-    #     cells.append(cell[1])
-    # print(cells)
-    # print("this is how many cells there are", len(cells))
+    cell_num = np.load(DATA + 'cell_ouput13k.npy')
+    whichCell = 0
+    for i in range(18, 44, 1):
+        targetLabel = getOneHotLabel(i, 271)
+        for j in range(500):
+            if targetLabel != cell_num[whichCell]:
+                print("This is the target label", targetLabel)
+                print("This is in the data", cell_num[whichCell])
+                whichCell += 1
 
-
-
-
-
-
-
-    # labels= np.load(DATA+ 'lstm_Heading_Output.npy')
-    # cell_frame_dict = np.load(DATA+ 'cell_origFrames.npy',allow_pickle='TRUE').item()
-    # frame_label = getFrameHeadingDict()
-    #
-    # ######################The actual hot labels!!!!!!!!!
-    # cel = '29'
-    # start = (int(cel)-18)*500
-    # hotLabel = []
-    # for i in range(start, start+500, 1):
-    #     hotLabel.append(labels[i])
-    # ##########################
-    #
-    # #########################The frames
-    # wantedCells = ['18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34',
-    #                '35', '36', '37', '38', '39', '40', '41', '42']
-    # frame_dict = OrderedDict()
-    # for cell in wantedCells:
-    #     frame_dict[cell] = cell_frame_dict[cell]
-    # #####################
-    #
-    #
-    # ############checking that they are the same
-    # which = 0
-    # for frame in frame_dict[cel]:
-    #     head = frame_label[frame]
-    #     onehot = [0] * 8
-    #     onehot[int(head)//45] = 1
-    #     onehot = np.asarray(onehot)
-    #     if (onehot != hotLabel[which]).all():
-    #         print(frame)
-    #     which +=1
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-
-
-
-#image = image[:,:,:,0]
-#
-# print("This is the length of image", len(image))
-#
-# cnt = 480
-# while (cnt != 12001):
-#     img = image[cnt]
-#     font = cv2.FONT_HERSHEY_SIMPLEX
-#     img = cv2.putText(np.float32(img), str(cnt), (10,50), font, 1, (255, 255, 255), 5, cv2.LINE_AA)
-#     cv2.imshow("Window", img)
-#     cv2.waitKey(0)
-#     cnt +=1
-# cv2.destroyAllWindows()
