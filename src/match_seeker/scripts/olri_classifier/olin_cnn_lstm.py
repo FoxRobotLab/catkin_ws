3c7f4b77ceff8230be52aa6a562c17efbbd871eb
analeidibarrera <abarrera@macalester.edu>
2020-07-13 14:27:08 -0500

    putting inputs correctly

--- src/match_seeker/scripts/olri_classifier/olin_cnn_lstm.py
+++ src/match_seeker/scripts/olri_classifier/olin_cnn_lstm.py
@@ -120,7 +120,8 @@ def image_cell_lstm(self):
         model.pop()
     for layer in range(9):
         model.layers[layer] = False
-    new_model.add(keras.layers.TimeDistributed(model.layers))
+    new_model.add(keras.layers.TimeDistributed(model.layers[0], input_shape= [None, 100, 100, 1]))
+    new_model.add(keras.layers.TimeDistributed(model.layers[1:]))
     new_model.add(keras.layers.TimeDistributed(keras.layers.Flatten()))
     new_model.add(keras.layers.LSTM(5))
     new_model.add(keras.layers.Dense(num_classes, activation='sigmoid'))
