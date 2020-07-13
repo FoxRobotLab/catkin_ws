139c73bdae2629a7b7717744f2ccb7248b7360ee
analeidibarrera <abarrera@macalester.edu>
2020-07-12 19:32:34 -0500

    filter increase

--- src/match_seeker/scripts/olri_classifier/olin_cnn_lstm.py
+++ src/match_seeker/scripts/olri_classifier/olin_cnn_lstm.py
@@ -75,7 +75,7 @@ def image_head_predCell(self):
     )))
     cnn.add(keras.layers.TimeDistributed(keras.layers.Dropout(0.4)))
     cnn.add(keras.layers.TimeDistributed(keras.layers.Conv2D(
-            filters=32,
+            filters=64,
             kernel_size=(5, 5),
             strides=(1, 1),
             activation="relu",
