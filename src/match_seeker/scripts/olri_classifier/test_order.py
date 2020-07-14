ed1a16800ec90b824e2eb6b7cb5d36f49ecc46e9
analeidibarrera <abarrera@macalester.edu>
2020-07-14 09:56:47 -0500

    debug

--- src/match_seeker/scripts/olri_classifier/test_order.py
+++ src/match_seeker/scripts/olri_classifier/test_order.py
@@ -13,8 +13,8 @@ def getOneHotLabel(number,size):
 if __name__ == '__main__':
     dict = np.load(DATA + "badDict.npy",allow_pickle='TRUE').item()
     print(type(dict['18']))
-    print(dict['18'].shape)
-
+    print(dict['18'])
+ 
 
 
 
