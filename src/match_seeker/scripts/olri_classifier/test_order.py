914622c11941c64dea490086145afc8737961f66
analeidibarrera <abarrera@macalester.edu>
2020-07-14 09:49:17 -0500

    debug

--- src/match_seeker/scripts/olri_classifier/test_order.py
+++ src/match_seeker/scripts/olri_classifier/test_order.py
@@ -11,17 +11,20 @@ def getOneHotLabel(number,size):
 
 
 if __name__ == '__main__':
+    dict = np.load(DATA + "badDict.npy")
+
+    print(dict['18'][0])
 
     #This is testing that the labels are correctly labeled!
-    cell_num = np.load(DATA + 'cell_ouput13k.npy')
-    whichCell = 0
-    for i in range(18, 44, 1):
-        targetLabel = getOneHotLabel(i, 271)
-        for j in range(500):
-            if (targetLabel != cell_num[whichCell]).all():
-                print("This is the target label", targetLabel)
-                print("This is in the data", cell_num[whichCell])
-                whichCell += 1
-
-    print("The cells are correct!")
+    # cell_num = np.load(DATA + 'cell_ouput13k.npy')
+    # whichCell = 0
+    # for i in range(18, 44, 1):
+    #     targetLabel = getOneHotLabel(i, 271)
+    #     for j in range(500):
+    #         if (targetLabel != cell_num[whichCell]).all():
+    #             print("This is the target label", targetLabel)
+    #             print("This is in the data", cell_num[whichCell])
+    #             whichCell += 1
+    #
+    # print("The cells are correct!")
 
