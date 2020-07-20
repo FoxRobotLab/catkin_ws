a4a9463e60de9f1c6702b33e1a61a1010536d904
arifzamil <azamil@macalester.edu>
2020-07-20 11:44:24 -0700

    Stuff

--- src/match_seeker/scripts/olri_classifier/preprocessData.py
+++ src/match_seeker/scripts/olri_classifier/preprocessData.py
@@ -407,8 +407,8 @@ class DataPreprocess(object):
 
         cellBorders = self._readCells(basePath + cellMapData)
 
-        for cell in cellBorders:
-            [x1, y1, x2, y2] = self.cellData[cell]
+        for cell in self.cellData:
+            [x1, y1, x2, y2] = cellBorders[cell]
             if (x1 <= x < x2) and (y1 <= y < y2):
                 return cell
 
