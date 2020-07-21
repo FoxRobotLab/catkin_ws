346c9f65da5ce5ea63f6cd184d7888319b807d85
analeidibarrera <abarrera@macalester.edu>
2020-07-21 08:48:10 -0500

    debug

--- src/match_seeker/scripts/olri_classifier/olri_training_w_generator.py
+++ src/match_seeker/scripts/olri_classifier/olri_training_w_generator.py
@@ -17,10 +17,12 @@ def gettingFrames():
     id = np.empty(0, dtype=int)
     for cell in cell_frame_dict.keys():
         frame_per_cell = frame_per_cell + cell_frame_dict[cell] + rndUnderRepSubset[cell]
-        id = np.hstack((id, np.zeros(len(cell_frame_dict[cell]), dtype = int), np.ones(rndUnderRepSubset[cell], dtype = int)))
+        id = np.hstack((id, np.zeros(len(cell_frame_dict[cell]), dtype = int), np.ones(len(rndUnderRepSubset[cell]), dtype = int)))
 
         print("frame", len(frame_per_cell))
+        print(len(cell_frame_dict[cell]))
         print("id",len(id))
+        print(len(rndUnderRepSubset[cell]))
         break
     frame = np.vstack((np.asarray(frame_per_cell), id))
     # print(frame.shape)
