f51b70f6dc18cb50411a1b0b0fa2479edd8d4700
analeidibarrera <abarrera@macalester.edu>
2020-07-21 08:37:54 -0500

    training with generator

---
+++ src/match_seeker/scripts/olri_classifier/olri_training_w_generator.py
@@ -0,0 +1,35 @@
+import numpy as np
+from paths import DATA
+
+from tensorflow import keras
+from data_per_epoch_gen import DataGenerator
+
+"""The data is currently not being organized, but this should be 
+done for LSTMs"""
+
+
+#WE EVENTUALLY WANT TO USE A GENERATOR FOR THIS
+def gettingFrames():
+    cell_frame_dict = np.load(DATA+ 'cell_origFrames.npy',allow_pickle='TRUE').item()
+    rndUnderRepSubset = np.load(DATA + 'cell_newFrames.npy', allow_pickle='TRUE').item()
+
+    frame_per_cell = []
+    id = np.empty(0, dtype=int)
+    for cell in cell_frame_dict.keys():
+        frame_per_cell = frame_per_cell + cell_frame_dict[cell] + rndUnderRepSubset[cell]
+        id = np.hstack(id, np.zeros(len(cell_frame_dict[cell])), np.ones(rndUnderRepSubset[cell]))
+        break
+    frame = np.vstack(np.asarray(frame_per_cell), id)
+    print(frame.shape)
+    print("These are the frames", frame[0])
+    print("These are the ids", frame[1])
+
+
+
+
+
+
+if __name__ == '__main__':
+    gettingFrames()
+
+
