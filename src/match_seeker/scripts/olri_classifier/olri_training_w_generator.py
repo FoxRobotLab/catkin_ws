25119e139b9857cd927886ee3ad5fa1b027a34fb
analeidibarrera <abarrera@macalester.edu>
2020-07-21 09:56:59 -0500

    debug

--- src/match_seeker/scripts/olri_classifier/olri_training_w_generator.py
+++ src/match_seeker/scripts/olri_classifier/olri_training_w_generator.py
@@ -45,13 +45,15 @@ def getLabels(cell = None, head = None, frames = None):
             frame_head_dict[frame] = split[-1]
     print("cell dict", len(frame_cell_dict))
     print("head dict", len(frame_head_dict))
-    
+
     print("frames length", len(frames))
     for frm in frames:
+        print("frame", frm)
         if cell == True:
             cellLabel.append(frame_cell_dict[frm])
         if head == True:
             headLabel.append(frame_head_dict[frm])
+            print("head", frame_head_dict[frm])
         break
 
     return cellLabel, headLabel
