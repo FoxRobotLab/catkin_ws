c40b5f381cadd33ed87aa2611afdaf9a4f32ec48
analeidibarrera <abarrera@macalester.edu>
2020-07-21 08:59:03 -0500

    debug

--- src/match_seeker/scripts/olri_classifier/olri_training_w_generator.py
+++ src/match_seeker/scripts/olri_classifier/olri_training_w_generator.py
@@ -13,17 +13,23 @@ def gettingFrames():
     cell_frame_dict = np.load(DATA+ 'cell_origFrames.npy',allow_pickle='TRUE').item()
     rndUnderRepSubset = np.load(DATA + 'cell_newFrames.npy', allow_pickle='TRUE').item()
 
-    frame_per_cell = []
-    id = np.empty(0, dtype=int)
-    num = ['37']
-    for cell in num:
-        frame_per_cell = frame_per_cell + cell_frame_dict[cell] + rndUnderRepSubset[cell]
-        id = np.hstack((id, np.zeros(len(cell_frame_dict[cell]), dtype = int), np.ones(len(rndUnderRepSubset[cell]), dtype = int)))
-        break
-    frame = np.vstack((np.asarray(frame_per_cell), id))
-    print(frame.shape)
-    print("These are the frames", frame[0])
-    print("These are the ids", frame[1])
+    for cell in cell_frame_dict.keys():
+        if(len(cell_frame_dict[cell]) < 500):
+            print(cell)
+            break
+
+
+    # frame_per_cell = []
+    # id = np.empty(0, dtype=int)
+    # num = ['']
+    # for cell in num:
+    #     frame_per_cell = frame_per_cell + cell_frame_dict[cell] + rndUnderRepSubset[cell]
+    #     id = np.hstack((id, np.zeros(len(cell_frame_dict[cell]), dtype = int), np.ones(len(rndUnderRepSubset[cell]), dtype = int)))
+    #     break
+    # frame = np.vstack((np.asarray(frame_per_cell), id))
+    # print(frame.shape)
+    # print("These are the frames", frame[0])
+    # print("These are the ids", frame[1])
 
 
 
