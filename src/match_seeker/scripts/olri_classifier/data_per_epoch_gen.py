751a7ba7cf35835dcbe22766d5943d32370c43bf
analeidibarrera <abarrera@macalester.edu>
2020-07-22 10:00:35 -0500

    data generator

--- src/match_seeker/scripts/olri_classifier/data_per_epoch_gen.py
+++ src/match_seeker/scripts/olri_classifier/data_per_epoch_gen.py
@@ -1,76 +1,102 @@
 import numpy as np
 from paths import DATA
 from tensorflow import keras
+import random
 import cv2
 
 
+
 mean = np.load(DATA + "lstm_mean_122k.npy")
+class DataGenerator(object):
+    def __init__(self, list_frames, labels, batch_size=20, dim=(100,100), n_channels=1, n_classes=10, shuffle=True,
+                 img_size = 100):
+        self.labels = labels
+        self.list_frames = list_frames
+        self.batch_size = batch_size
+        self.dim = dim
+
+        #######
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        ########
+
+        self.shuffle = shuffle
+        self.img_size = img_size
+        self.on_epoch_end()
+        self.image_path = DATA + 'frames/moreframes/frame'
 
 
-def __init__(self, list_IDs, labels, batch_size=20, dim=(100,100,1), n_channels=1, n_classes=10, shuffle=True,
-             img_size = 100):
- self.img_size = img_size
- self.dim = dim
- self.batch_size = batch_size
- self.labels = labels
- self.list_IDs = list_IDs
- self.n_channels = n_channels
- self.n_classes = n_classes
- self.shuffle = shuffle
- self.on_epoch_end()
+    def __len__(self):
+      'Denotes the number of batches per epoch'
+      return int(np.floor(len(self.list_frames) / self.batch_size))
 
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.list_frames))
 
-def __len__(self):
-  'Denotes the number of batches per epoch'
-  #IF THIS WAS TAKING THE WHOLE DATA THEN 122,000/22 ----> 24,400 IS THE BATCH NUMBERS THAT HAVE TO BE DONE???
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+        #THIS SHOULD ALSO BE WHERE WE SAVE DATA
 
-  #HOWEVER, WE WANT THIS TO TAKE IN THE VAL AND TRAIN DATA WHEN DOING BATCHES, TRUE IN TOTAL WE WILL WANT TO GO THROUGH
-  #24,000
-  return int(np.floor(len(self.list_IDs) / self.batch_size))
+    def __getitem__(self, index):
+      'Generate one batch of data'
+      indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
 
-def __getitem__(self, index):
-  'Generate one batch of data'
-  # Generate indexes of the batch
-  indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+      # Find list of frames
+      list_frame_temp = [self.list_frames[k] for k in indexes]
 
-  # Find list of IDs
-  list_IDs_temp = [self.list_IDs[k] for k in indexes]
+      # Generate data
+      X, y = self.__data_generation(list_frame_temp)
 
-  # Generate data
-  X, y = self.__data_generation(list_IDs_temp)
+      return X, y
 
-  return X, y
+    def __data_generation(self, list_frame_temp):
+        'Generates data containing batch_size images'
+        # Initialization
+        X = np.empty((self.batch_size, *self.dim, self.n_channels)) #IS AN ARRAY WITHOUT INITIALIZING THE ENTRIES OF SHAPE (20, 100, 100, 1, 1)
+        y = np.empty((self.batch_size), dtype=int)
+        # Generate data
+        for i, frm in enumerate(list_frame_temp):
+            # Store sample
+            X[i,] = self._load_grayscale_image(self.image_path + frm[0]+ '.jpg', frm[1]) #Array of images
 
-def on_epoch_end(self):
-    'Updates indexes after each epoch'
-    self.indexes = np.arange(len(self.list_IDs))
-    if self.shuffle == True:
-        np.random.shuffle(self.indexes)
-    #THIS SHOULD ALSO BE WHERE WE SAVE DATA
+            # Store class
+            y[i] = self.labels[frm]
 
-def __data_generation(self, list_IDs_temp):
-    'Generates data containing batch_size images'
-    # Initialization
-    X = np.empty((self.batch_size, *self.dim, self.n_channels)) #IS AN ARRAY WITHOUT INITIALIZING THE ENTRIES OF SHAPE (32, 100, 100, 1, 1)
-    y = np.empty((self.batch_size), dtype=int)
-    # Generate data
-    for i, ID in enumerate(list_IDs_temp):
-        # Store sample
-        X[i,] = _load_grayscale_image(self.image_path + self.labels[ID]+ '.jpg') #Array of images
+        return X, keras.utils.to_categorical(y, num_classes=self.n_classes) #Array of labels
 
-        # Store class
-        y[i] = self.labels[ID]
+    def _load_grayscale_image(self, image_path, typeOfProcessing):
+        img = cv2.imread(image_path)
+        img = cv2.resize(img, (self.img_size, self.img_size))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        if typeOfProcessing == 1:
+            img = self.randerase_image(img)
+        img = img - mean
+        img = img / 255
+        return img
 
-    return X, keras.utils.to_categorical(y, num_classes=self.n_classes) #Array of labels
+    def randerase_image(self, image, erase_ratio, size_min=0.02, size_max=0.4, ratio_min=0.3, ratio_max=1 / 0.3, val_min=0,
+                        val_max=255):
+        """ Randomly erase a rectangular part of the given image in order to augment data
+        https://github.com/yu4u/cutout-random-erasing/blob/master/random_eraser.py"""
+        re_image = image.copy()
+        h, w = re_image.shape
+        er = random.random()  # a float [0.0, 1.0)
+        if er > erase_ratio:
+            return None
 
-def _load_grayscale_image(self, image_path):
-    img = cv2.imread(image_path)
-    img = cv2.resize(img, (self.img_sizeimg_size, self.img_size))
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = img - mean
-    img = img / 255
-
-    return img
+        while True:
+            size = np.random.uniform(size_min, size_max) * h * w
+            ratio = np.random.uniform(ratio_min, ratio_max)
+            width = int(np.sqrt(size / ratio))
+            height = int(np.sqrt(size * ratio))
+            left = np.random.randint(0, w)
+            top = np.random.randint(0, h)
+            if (left + width <= w and top + height <= h):
+                break
+        color = np.random.uniform(val_min, val_max)
+        re_image[top:top + height, left:left + width] = color
+        return re_image
 
 
 
--- src/match_seeker/scripts/olri_classifier/olri_training_w_generator.py
+++ src/match_seeker/scripts/olri_classifier/olri_training_w_generator.py
@@ -118,7 +118,7 @@ if __name__ == '__main__':
     data_dict = olri_locator.trainAndEval()
 
     # Parameters
-    params = {'dim': (100, 100, 1),
+    params = {'dim': (100, 100),
               'batch_size': 24,
               'n_channels': 1,
               'shuffle': True}
