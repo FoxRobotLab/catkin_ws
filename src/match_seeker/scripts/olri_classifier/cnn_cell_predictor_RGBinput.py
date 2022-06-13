7cda4ae828b51d8db199960cc03d5fe28f3fdf3b
Yifan Wu <ywu@macalester.edu>
2022-06-13 15:31:34 -0500

    Cleaned up unused variables, made the test function compatible

--- src/match_seeker/scripts/olri_classifier/cnn_cell_predictor_RGBinput.py
+++ src/match_seeker/scripts/olri_classifier/cnn_cell_predictor_RGBinput.py
@@ -9,13 +9,12 @@ The model was originally created in 2019 that takes a picture and its heading as
 FULL TRAINING IMAGES LOCATED IN match_seeker/scripts/olri_classifier/frames/moreframes
 --------------------------------------------------------------------------------"""
 
-import os
-import random
 import time
-
-import numpy as np
 import cv2
 import tensorflow as tf
+
+import os
+import numpy as np
 from tensorflow import keras
 import cv2
 import time
@@ -24,21 +23,16 @@ from imageFileUtils import makeFilename, extractNum
 from frameCellMap import FrameCellMap
 from DataGenerator2022 import DataGenerator2022
 import random
-# from sklearn.model_selection import train_test_split
-#from preprocessData import DataPreprocess
-
-
 
 ### Uncomment next line to use CPU instead of GPU: ###
 #os.environ['CUDA_VISIBLE_DEVICES'] = ''
 
 class CellPredictor2019(object):
-    def __init__(self, checkPointFolder = checkPts, eval_ratio=11.0 / 61.0, loaded_checkpoint="2022CellPredict_checkpoint-0610221632/FullData-20-0.80.hdf5", imagesFolder=frames, imagesParent=DATA+"frames/",
-                 labelMapFile=DATA + "frames/MASTER_CELL_LOC_FRAME_IDENTIFIER.txt", outputSize=271, image_size=100, image_depth=3, data_name=None, dataSize = 0,
-                 testData=DATA, batch_size = 32, seed=123456):
+    def __init__(self, checkPointFolder = None, loaded_checkpoint = None, imagesFolder = None, imagesParent = None, labelMapFile = None, data_name=None,
+                 eval_ratio=11.0 / 61.0, outputSize=271, image_size=100, image_depth=3, dataSize = 0, batch_size = 32, seed=123456):
+
         ### Set up paths and basic model hyperparameters
         # TODO: We need to add a comment here that describes exactly what each of these inputs means!!
-
         self.checkpoint_dir = checkPointFolder + "2022CellPredict_checkpoint-{}/".format(time.strftime("%m%d%y%H%M"))
         self.outputSize = outputSize
         self.eval_ratio = eval_ratio
@@ -53,12 +47,8 @@ class CellPredictor2019(object):
         # self.eval_images = None
         # self.eval_labels = None
         self.data_name = data_name
-        self.dataSize = dataSize
-
-        self.testData = testData
+        self.dataSize = dataSize #len of ...?
         self.loaded_checkpoint = loaded_checkpoint
-        self.mean_image = self.testData + "TRAININGDATA_100_500_mean.npy"
-        self.frameIDtext = self.testData + "frames/MASTER_CELL_LOC_FRAME_IDENTIFIER.txt"
         self.frames = imagesFolder
         self.framesParent = imagesParent
         self.labelMapFile = labelMapFile
@@ -68,10 +58,13 @@ class CellPredictor2019(object):
         if loaded_checkpoint:
             self.loaded_checkpoint = checkPointFolder + loaded_checkpoint
 
+    def buildMap(self):
+        self.labelMap = FrameCellMap(dataFile=self.labelMapFile)
+
     # def prepDatasets(self):
     #     """Finds the cell labels associated with the files in the frames folder, and then sets up two
     #     data generators to produce the data in batches."""
-    #     self.labelMap = FrameCellMap(dataFile=self.labelMapFile)  # imageDir=self.frames Susan added this
+    #     self.buidMap
     #     files = [f for f in os.listdir(self.frames) if f.endswith("jpg")]
     #     cellLabels = [self.labelMap.frameData[fNum]['cell'] for fNum in map(extractNum, files)]
     #     self.train_ds = keras.utils.image_dataset_from_directory(self.framesParent, labels=cellLabels, subset="training",
@@ -94,7 +87,6 @@ class CellPredictor2019(object):
             self.model = keras.models.load_model(self.loaded_checkpoint) #, compile=True)
             self.model.load_weights(self.loaded_checkpoint)
         else:
-            # self.model = self.cnn_headings()  # CNN
             self.model = self.cnn()  # CNN
 
         self.model.compile(
@@ -102,21 +94,7 @@ class CellPredictor2019(object):
             optimizer=keras.optimizers.SGD(lr=self.learning_rate),
             metrics=["accuracy"])
 
-
-    def _processFile(self, filenameTensor):
-        """Takes in a Tensor holding the filename, and extracts it, reading in the image
-        and also extracting the frame number and looking up its cell"""
-        filename = filenameTensor.numpy()
-        print(filename)
-        justFname = filename.split('/')[-1]
-        frameNum = extractNum(justFname)
-        cellNum = self.labelMap.frameData[frameNum]['cell']
-        image = cv2.imread(filename)
-        assert image is not None
-        return (image, cellNum)
-
-
-    # def train(self):
+    # def train(self, epochs = 20):
     #     """Sets up the loss function and optimizer, and then trains the model on the current training data. Quits if no
     #     training data is set up yet."""
     #
@@ -128,7 +106,7 @@ class CellPredictor2019(object):
     #     self.model.fit(
     #         self.train_images, self.train_labels,
     #         batch_size= 1,
-    #         epochs=20,
+    #         epochs=epochs,
     #         verbose=1,
     #         validation_data=(self.eval_images, self.eval_labels),
     #         shuffle=True,
@@ -179,6 +157,8 @@ class CellPredictor2019(object):
         """Builds a network that takes an image and produces the cell number."""
 
         model = keras.models.Sequential()
+
+        #rescale and resize layers not compatible with tensorflow 1, so they're done in prepDatasets function.
         # model.add(keras.layers.Rescaling(scale = 1./255, offset=0.0, **kwargs))
         # model.add(keras.layers.Resizing(
         #         self.image_size, self.image_size, interpolation="bilinear", crop_to_aspect_ratio=False, **kwargs))
@@ -249,15 +229,12 @@ class CellPredictor2019(object):
                 topVals.pop(-1)
         return topVals, topIndex
 
-    def test(self, n, randomChoose = True, randomCrop = False):
+    def test(self, n, randomChoose = True):
         """This runs each of the random n images in the folder of frames through the cell-output network, reporting how
         often the correct cell was produced0, and how often the correct heading was in the top 3 and top 5.
-        Or when setting random to be false, it tests the model on the n-th image."""
+        Or when setting randomChoose to be false, it tests the model on the n-th image."""
 
-        potentialHeadings = [0, 45, 90, 135, 180, 225, 270, 315, 360]
-        mean = np.load(self.mean_image)
-        print("Setting up preprocessor to get frame data...")
-        dPreproc = FrameCellMap(dataFile=self.frameIDtext)
+        dPreproc = FrameCellMap(dataFile=self.labelMapFile)
         countPerfect = 0
         countTop3 = 0
         countTop5 = 0
@@ -268,23 +245,16 @@ class CellPredictor2019(object):
             if randomChoose:
                 index = random.randrange(95000)-1
             else: index = n - 1
-            print("===========", index)
+            print("===========", index+1)
             imFile = makeFilename(self.frames, index)
             imageB = cv2.imread(imFile)
             if imageB is None:
                 print(" image not found")
                 continue
             cellB = dPreproc.frameData[index]['cell']
-            headingB = dPreproc.frameData[index]['heading']
-            headingIndex = potentialHeadings.index(headingB)  #converting from 0, 45, 90, etc. to 0, 1, 2, etc.
 
-            if randomCrop:
-                smallerB, grayB, processedB = self.cleanImageRandomCrop(imageB, mean)
-            else:
-                smallerB, grayB, processedB = self.cleanImage(imageB, mean)
-            headBArr = headingIndex * np.ones((100, 100, 1))
-            procBPlus = np.concatenate((np.expand_dims(processedB, axis=-1), headBArr), axis=-1)
-            predB, output = self.predictSingleImageAllData(procBPlus)
+            processedB = self.cleanImage(imageB)
+            predB, output = self.predictSingleImageAllData(processedB)
             topThreePercs, topThreeCells = self.findTopX(3, output)
             topFivePercs, topFiveCells = self.findTopX(5, output)
             print("cellB =", cellB, "   predB =", predB)
@@ -296,39 +266,21 @@ class CellPredictor2019(object):
                 countTop3 += 1
             if cellB in topFiveCells:
                 countTop5 += 1
-            dispProcB = cv2.convertScaleAbs(processedB)
             cv2.imshow("Image B", cv2.resize(imageB, (400, 400)))
             cv2.moveWindow("Image B", 50, 50)
-            cv2.imshow("Smaller B", cv2.resize(smallerB, (400, 400)))
-            cv2.moveWindow("Smaller B", 50, 500)
-            cv2.imshow("Gray B", cv2.resize(grayB, (400, 400)))
-            cv2.moveWindow("Gray B", 500, 500)
-            cv2.imshow("Proce B", cv2.resize(dispProcB, (400, 400)))
-            cv2.moveWindow("Proce B", 500, 50)
             x = cv2.waitKey(50)
             if chr(x & 0xFF) == 'q':
                 break
         print("Count of perfect:", countPerfect)
         print("Count of top 3:", countTop3)
         print("Count of top 5:", countTop5)
-    #
-    # def cleanImage(self, image, mean=None, imageSize=100):
-    #     """Preprocessing the images in similar ways to the training dataset of 2019 model."""
-    #     shrunkenIm = cv2.resize(image, (imageSize, imageSize))
-    #     grayed = cv2.cvtColor(shrunkenIm, cv2.COLOR_BGR2GRAY)
-    #     meaned = np.subtract(grayed, mean)
-    #     return shrunkenIm, grayed, meaned
-    #
-    # def cleanImageRandomCrop(self, image, mean=None, imageSize=100):
-    #     """Alternative preprocessing function to cleanImage that crops the input image to a 100x100 image starting
-    #     at a random x and y position. Resulted in very bad performance, we might not use anymore."""
-    #     image = cv2.resize(image, (170, 128))
-    #     x = random.randrange(0, 70)
-    #     y = random.randrange(0, 28)
-    #     cropped_image = image[y:y + imageSize, x:x + imageSize]
-    #     grayed = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
-    #     meaned = np.subtract(grayed, mean)
-    #     return cropped_image, grayed, meaned
+
+
+    def cleanImage(self, image, imageSize=100):
+        """Preprocessing the images in similar ways to the training dataset of 2019 model."""
+        shrunkenIm = cv2.resize(image, (imageSize, imageSize))
+        processedIm = shrunkenIm / 255.0
+        return processedIm
 
 # def loading_bar(start,end, size = 20):
 #     # Useful when running a method that takes a long time
@@ -336,39 +288,25 @@ class CellPredictor2019(object):
 #     if start % 10 == 0:
 #         print(loadstr)
 
-
-# def check_data():
-#     data = np.load(DATA + 'TRAININGDATA_100_500_heading-input_gnrs.npy')
-#     print(data.shape)
-#     np.random.shuffle(data)
-#     print(data[0])
-#     potentialHeadings = [0, 45, 90, 135, 180, 225, 270, 315, 360]
-#     for i in range(len(data)):
-#         print("cell:"+str(np.argmax(data[i][1])))
-#         print("heading:"+str(potentialHeadings[int(data[i][0][0,0,1])]))
-#         cv2.imshow('im',data[i][0][:,:,0])
-#         cv2.moveWindow('im',200,200)
-#         cv2.waitKey(0)
-
-
 if __name__ == "__main__":
     #check_data()
     cellPredictor = CellPredictor2019(
-        # for setting up for building and training model
-        # eval_ratio = 0.2,
-        # checkPts = DATA + "CHECKPOINTS",    # TODO: check this not sure about it
-        # imagesFolder = DATA + "frames/moreFrames/",
-        # labelMapFile = DATA + "frames/MASTER_CELL_LOC_FRAME_IDENTIFIER.txt",
-        # loaded_checkpoint=False,
-        # dataSize=95810
-        # data_name = "testCellPredictor"
-        data_name="FullData"
+        # dataSize=95810,
+        data_name="FullData",
+        checkPointFolder=checkPts,
+        loaded_checkpoint="2022CellPredict_checkpoint-0610221632/FullData-20-0.80.hdf5",
+        imagesFolder=frames,
+        imagesParent=DATA + "frames/",
+        labelMapFile=DATA + "frames/MASTER_CELL_LOC_FRAME_IDENTIFIER.txt",
     )
 
     cellPredictor.buildNetwork()
-    #cellPredictor.prepDatasets()
 
+    #for newer Tensorflow versions:
+    #cellPredictor.prepDatasets()
+    #cellPredictor.train_withGenerator(cellPredictor.train_ds, cellPredictor.val_ds)
+
+    #for Tensorflow 1
     training_generator = DataGenerator2022()
     validation_generator = DataGenerator2022(train = False)
-    #cellPredictor.train_withGenerator(cellPredictor.train_ds, cellPredictor.val_ds)
     cellPredictor.train_withGenerator(training_generator,validation_generator)
