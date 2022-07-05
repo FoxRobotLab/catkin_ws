3c82f8e35287fd63a08d4aae90b4ff77b8cae28c
susaneileenfox <fox@macalester.edu>
2022-07-05 10:58:53 -0500

    updated code to use VideoWriter

--- src/match_seeker/scripts/buildingDatabases/classVideoToFiles.py
+++ src/match_seeker/scripts/buildingDatabases/classVideoToFiles.py
@@ -12,18 +12,24 @@ def saveVideo(destDir):
     videoName = destDir + '/' + timestamp + ".avi"
     frameFolder = destDir + '/' + timestamp + 'frames/'
     os.mkdir(frameFolder)
-    capt = cv2.VideoCapture(videoName)
+    # TODO: Get a frame from somewhere so we can ask about its size
+    (hgt, wid, _) = frame.shape
+    fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+    vidWriter = cv2.VideoWriter(videoName, fourcc, 25.0, (wid, hgt), 1)  # TODO: Check these parameters
     frameNum = 0
+    counter = 0
     while True:
-        res, frame = capt.read()
-        if not res:
-            break
+        frame = # Get frame from ROS here
+        vidWriter.write(frame)
         cv2.imshow("frames", frame)
         cv2.waitKey(10)
-        saveToFolder(frame, frameFolder, frameNum)
-        frameNum += 1
-    capt.release()
-    # cv2.destroyAllWindows()
+        counter += 1
+        if counter == 10:
+            saveToFolder(frame, frameFolder, frameNum)
+            frameNum += 1
+            counter = 0
+    vidWriter.release()
+
 
 
 def saveToFolder(img, folderName, frameNum):
