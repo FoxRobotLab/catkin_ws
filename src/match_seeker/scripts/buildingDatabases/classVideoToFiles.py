8beb954511aacc48953102ab15430be9ccbefa79
susaneileenfox <fox@macalester.edu>
2022-07-05 10:16:54 -0500

    changed code to save video to file

--- src/match_seeker/scripts/buildingDatabases/classVideoToFiles.py
+++ src/match_seeker/scripts/buildingDatabases/classVideoToFiles.py
@@ -1,20 +1,27 @@
 
 import os
+import time
+import datetime
 import cv2
 
-def saveVideo(videoName, destDir):
+
+def saveVideo(destDir):
     """Takes in the name of a video file and the name of a file classifying the video frames into
     categories, and it saves frames from the video into separate folders based on classification."""
+    timestamp = datetime.now().strftime("%Y%m%d-%H:%M")
+    videoName = destDir + '/' + timestamp + ".avi"
+    frameFolder = destDir + '/' + timestamp + 'frames/'
+    os.mkdir(frameFolder)
     capt = cv2.VideoCapture(videoName)
     frameNum = 0
     while True:
         res, frame = capt.read()
         if not res:
             break
-        #cv2.imshow("frames",frames)
-        #cv2.waitKey(10)
-        saveToFolder(frame, destDir, frameNum)
-        frameNum+=1
+        cv2.imshow("frames", frame)
+        cv2.waitKey(10)
+        saveToFolder(frame, frameFolder, frameNum)
+        frameNum += 1
     capt.release()
     # cv2.destroyAllWindows()
 
@@ -34,5 +41,5 @@ def nextFilename(num):
     return fileName
 
 
-saveVideo("/home/macalester/turtlebot_videos/biology_west_atrium.avi", "/home/macalester/turtlebot_videos/biologyWestAtriumFrames/")
+
 
