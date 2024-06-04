bba0b8e134a9f8307320cde47135296de72a3f5a
Jinyoung Lim <jlim2@macalester.edu>
2024-06-04 10:45:14 -0500

    Adding paths for compatibility

---
+++ src/match_seeker/scripts/olri_classifier/paths.py
@@ -0,0 +1,7 @@
+pathToMatchSeeker = '/home/macalester/PycharmProjects/catkin_ws/src/match_seeker/'
+frames = '/home/macalester/PycharmProjects/catkin_ws/src/match_seeker/res/classifier2019data/DATA/frames/moreframes/'
+DATA = '/home/macalester/PycharmProjects/catkin_ws/src/match_seeker/res/classifier2019data/DATA/'
+checkPts = DATA + "CHECKPOINTS/"
+data2022 = '/home/macalester/PycharmProjects/catkin_ws/src/match_seeker/res/classifier2022Data/'
+data2022rename = '/home/macalester/Desktop/newData2022/classifier2022Data/'
+
