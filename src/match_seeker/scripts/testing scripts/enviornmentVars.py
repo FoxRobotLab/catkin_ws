1e981e0e143aecff07eb1eb8514fef7640d85b66
Shengyuan Wang <swang3@macalester.edu>
2024-06-10 16:23:53 -0500

    Script now works properly for ROS_HOSTNAME and ROS_MASTER_URI

--- src/match_seeker/scripts/testing scripts/enviornmentVars.py
+++ src/match_seeker/scripts/testing scripts/enviornmentVars.py
@@ -3,49 +3,6 @@ import subprocess
 import tempfile
 import socket
 
-
-# Adds the hostname to the .bashrc file
-def set_hostname(socketname):
-    bashrc_path = os.path.expanduser("~/.bashrc")
-    with open(bashrc_path, "a") as bashrc:
-        bashrc.write("\nexport ROS_HOSTNAME={}\n".format(socketname))
-
-    print "ROS_HOSTNAME set to {}".format(socketname)
-
-    # Creates temporary shell script to source the .bashrc file
-    with tempfile.NamedTemporaryFile(delete=False, mode='w', suffix='.sh') as temp_script:
-        temp_script.write("/bin/bash\n")
-        temp_script.write("source {}\n".format(bashrc_path))
-        temp_script.write("echo Current ROS_HOSTNAME: $ROS_HOSTNAME\n")
-        temp_script_path = temp_script.name
-
-    # Make temporary script executable, executes the temporary shell, and cleans it up
-    subprocess.call(['chmod', '+x', temp_script_path])
-    subprocess.call(['bash', '-c', temp_script_path])
-    os.remove(temp_script_path)
-
-
-# Adds the ros_master_uri to the .bashrc file
-def set_ros_master_uri(uri):
-    bashrc_path = os.path.expanduser("~/.bashrc")
-    with open(bashrc_path, "a") as bashrc:
-        bashrc.write("\nexport ROS_MASTER_URI={}\n".format(uri))
-
-    print "ROS_MASTER_URI set to {}".format(uri)
-
-    # Creates temporary shell script to source the .bashrc file
-    with tempfile.NamedTemporaryFile(delete=False, mode='w', suffix='.sh') as temp_script:
-        temp_script.write("/bin/bash\n")
-        temp_script.write("source {}\n".format(bashrc_path))
-        temp_script.write("echo Current ROS_MASTER_URI: $ROS_MASTER_URI\n")
-        temp_script_path = temp_script.name
-
-    # Make temporary script executable, executes the temporary shell, and cleans it up
-    subprocess.call(['chmod', '+x', temp_script_path])
-    subprocess.call(['bash', '-c', temp_script_path])
-    os.remove(temp_script_path)
-
-
 # Getting the computer's hostname and displaying it in the shell
 hostname = socket.gethostname()
 IPSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -65,5 +22,28 @@ else:
     print("Invalid robot name. Please try again.")
 
 
-set_ros_master_uri(new_uri)
-set_hostname(IPSocketName)
+# Adds the hostname and master uri to the .bashrc file
+def set_variables(uri, socketname):
+    bashrc_path = os.path.expanduser("~/.bashrc")
+    with open(bashrc_path, "a") as bashrc:
+        bashrc.write("\nexport ROS_MASTER_URI={}\n".format(uri))
+        bashrc.write("\nexport ROS_HOSTNAME={}\n".format(socketname))
+
+    print "ROS_MASTER_URI set to {}".format(uri)
+    print "ROS_HOSTNAME set to {}".format(socketname)
+
+    # Creates temporary shell script to source the .bashrc file
+    with tempfile.NamedTemporaryFile(delete=False, mode='w', suffix='.sh') as temp_script:
+        temp_script.write("/bin/bash\n")
+        temp_script.write("source {}\n".format(bashrc_path))
+        temp_script.write("echo Current ROS_MASTER_URI: $ROS_MASTER_URI\n")
+        temp_script.write("echo Current ROS_HOSTNAME: $ROS_HOSTNAME\n")
+        temp_script_path = temp_script.name
+
+    # Make temporary script executable, executes the temporary shell, and cleans it up
+    subprocess.call(['chmod', '+x', temp_script_path])
+    subprocess.call(['bash', '-c', temp_script_path])
+    os.remove(temp_script_path)
+
+
+set_variables(new_uri, IPSocketName)
