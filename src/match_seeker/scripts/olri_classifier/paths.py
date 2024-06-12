e46725c3537381ed7d57f2f824bf87e66b98a386
Shengyuan Wang <swang3@macalester.edu>
2024-06-12 09:54:55 -0500

    Added data files for new data

--- src/match_seeker/scripts/olri_classifier/paths.py
+++ src/match_seeker/scripts/olri_classifier/paths.py
@@ -3,6 +3,6 @@ frames = '/home/macalester/PycharmProjects/catkin_ws/src/match_seeker/res/classi
 DATA = '/home/macalester/PycharmProjects/catkin_ws/src/match_seeker/res/classifier2019data/DATA/'
 checkPts = DATA + "CHECKPOINTS/"
 logs = DATA + "LOGS/"
-data2022 = '/home/macalester/PycharmProjects/catkin_ws/src/match_seeker/res/classifier2022Data/'
+DATA2022 = '/home/macalester/PycharmProjects/catkin_ws/src/match_seeker/res/classifier2022Data/'
 data2022rename = '/home/macalester/Desktop/newData2022/classifier2022Data/'
 
