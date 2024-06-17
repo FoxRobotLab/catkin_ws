9694340c4cd01a5f10c97f0387c1514b14ac56de
Shengyuan Wang <swang3@macalester.edu>
2024-06-17 10:28:03 -0500

    removed errors in _writeInFile and _getCurrentImage in reviewImageData.py

--- src/match_seeker/scripts/testing scripts/reviewImageData.py
+++ src/match_seeker/scripts/testing scripts/reviewImageData.py
@@ -244,7 +244,7 @@ class ImageReview(object):
         folderPath = "../../res/classifier2022Data/DATA/FrameData/" + folder + "/"
         imageList = sorted(os.listdir(folderPath))
         image = imageList[self.currImageIndex]
-        self.currImageFolderLength = len(folder)
+        self.currImageFolderLength = len(imageList)
         return image
 
     def _writeInFile(self):
@@ -254,15 +254,11 @@ class ImageReview(object):
         # assign frame to current line of txt file
 
         currFile = open(self.currWritingFileName, "a")
-        # checks if the file being written to is empty
-        first_char = currFile.read(1)
-        if not first_char:
-            currFile.write(
-                self.currImage + " " + self.currLine[1] + " " + self.currLine[2] + " " + self.currLine[4] + " "
-                + self.currLine[3] + " " + self.currLine[0] + "\n")
-            currFile.close()
-        else:
-            print("File already has text in it. Please change its location.")
+        # TODO: Check if we do not want files to start blank every time
+        currFile.write(
+            self.currImage + " " + self.currLine[1] + " " + self.currLine[2] + " " + self.currLine[4] + " "
+            + self.currLine[3] + " " + self.currLine[0] + "\n")
+        currFile.close()
 
 
 if __name__ == "__main__":
