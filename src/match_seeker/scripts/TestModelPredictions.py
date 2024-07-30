d0e47786bec02d5747eacb86e835b585bdd7f4b9
oscarrezab <110639308+oscarrezab@users.noreply.github.com>
2024-07-30 14:45:58 -0500

    Modify TestModelPredictions.py to work with 2024 models

--- src/match_seeker/scripts/TestModelPredictions.py
+++ src/match_seeker/scripts/TestModelPredictions.py
@@ -4,8 +4,6 @@ robot alongside text stating the model's predictions on cell and heading.
 Currently works for the 2022 models.
 Works on Tensorflow 2.15.1 with Python 3.9, for reasons still unknown.
 
-TODO: add support for the 2024 models
-
 Created: Summer 2024
 Author: Oscar Reza B.
 ------------------------------------------------------------------------------------------------------------------- """
@@ -13,30 +11,42 @@ Author: Oscar Reza B.
 import os
 import cv2
 
-from src.match_seeker.scripts.olri_classifier.cnnRunModel import ModelRunRGB
+from olri_classifier.cnnRunModel import ModelRunLSTM
 import OlinWorldMap
 
 # Set data path for Precision 5820  # TODO: Consider adding option to set path for other devices
 mainPath = "/home/macalester/PycharmProjects/catkin_ws/src/match_seeker/res/classifier2022Data/DATA/"
 framesDataPath = mainPath + "FrameData/"
 
-# Load the model, currently for the 2022 models
-modelTester = ModelRunRGB()
+# Load the model
+modelTester = ModelRunLSTM()
 
 # Get the building map
 olinMap = OlinWorldMap.WorldMap()
 
 # Read in the folder path
-folderName = os.listdir(framesDataPath)[5]  # TODO: Make this a user-selected folder. Currently chose 6th folder in dataset, for no good reason.
+folderName = os.listdir(framesDataPath)[9]  # TODO: Make this a user-selected folder. Currently chose 10th folder in dataset, for no good reason.
 folderContents = sorted(os.listdir(os.path.join(framesDataPath, folderName)))
+
+imagesList = []
+cell = None
+heading = None
 
 # Go through every frame in the data collection run
 for frame in folderContents:
   # Read in the image
   image = cv2.imread(os.path.join(framesDataPath, folderName, frame))
 
+  if len(imagesList) != 10:
+    imagesList.append(image)
+    print(f"Cell prediction: {cell}")
+    print(f"Heading prediction: {heading}")
+    cv2.imshow("frame", image)
+    # cv2.waitKey(30)
+    continue
+
   # Get the predictions and store them as variables
-  scores, matchLocs = modelTester.getPrediction(image, olinMap)
+  scores, matchLocs = modelTester.getPrediction(imagesList, olinMap)
   prediction = matchLocs[0]
   x_coord = prediction[0]
   y_coord = prediction[1]
@@ -46,7 +56,12 @@ for frame in folderContents:
 
   # Print the predictions into the frame and display the frame
   image = cv2.putText(image, f"Cell prediction: {cell}", (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 3)
+  print(f"Cell prediction: {cell}")
   image = cv2.putText(image, f"Heading prediction: {heading}", (0, 60), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 3)
+  print(f"Heading prediction: {heading}")
   cv2.imshow("frame", image)
 
-  cv2.waitKey(10)
+  cv2.waitKey(30)
+
+  imagesList.pop(0)
+
