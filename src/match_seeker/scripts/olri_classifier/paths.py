f6da52b9b647c8ea4332393d9fc99cbf45a60920
oscarrezab <110639308+oscarrezab@users.noreply.github.com>
2024-08-08 15:34:47 -0500

    Minor path updates

--- src/match_seeker/scripts/olri_classifier/paths.py
+++ src/match_seeker/scripts/olri_classifier/paths.py
@@ -4,10 +4,16 @@ DATA = '/home/macalester/PycharmProjects/catkin_ws/src/match_seeker/res/classifi
 # checkPts = DATA + "CHECKPOINTS/"
 logs = DATA + "LOGS/"
 DATA2022 = '/home/macalester/PycharmProjects/catkin_ws/src/match_seeker/res/classifier2022Data/'
-DATA2022rename = '/home/macalester/Desktop/newData2022/classifier2022Data/'
+FRAMES2022 = DATA2022 + 'DATA/FrameData'
 
 DATA2022 = '/Users/susan/PycharmProjects/catkin_ws/src/match_seeker/res/classifier2022Data/'
 
+# These paths are for tower 5820
+myPath = "/home/macalester/PycharmProjects/catkin_ws/src/match_seeker/res/classifier2022Data/DATA/"
+textDataPath = myPath + "AnnotData/"
+framesDataPath = myPath + "FrameData/"
+checkPts = myPath + "CHECKPOINTS/"
+
 # Paths for T7 SSD
-DATA = '/Volumes/T7/macalester/classifier2022Data-220722/'
-checkPts = DATA + "CHECKPOINTS/"
+# DATA = '/Volumes/T7/macalester/classifier2022Data-220722/'
+# checkPts = DATA + "CHECKPOINTS/"
